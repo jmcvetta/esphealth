0c0c8526674ccc28f94957eff31f022e830bda03
rexua <rexua@localhost>
2006-11-02 19:55:40 +0000

    add preload ndc/icd9/cpt

--- ESP/utils/preLoader.py
+++ ESP/utils/preLoader.py
@@ -1,20 +1,21 @@
 ##preLoader is to load some data into ESP_conditionNDC table, ESP_conditionLOINC, ESP_CPTLOINCMAP table
 ##
-import  os
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-#os.environ['PYTHONPATH'] = '\home\ESP\django\ESP'
+import os,sys
+sys.path.insert(0, '/home/ESP/')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ESP.settings'
+
+
 import django, datetime
 from ESP.esp.models import *
 from django.db.models import Q
 from ESP.settings import TOPDIR
-import localconfig 
-import string
-import sys
+from ESP.utils import localconfig 
+import string,csv
 import traceback
 import StringIO
 
 logging = localconfig.getLogging('preLoader.py v0.1', debug=0)
-datadir = os.path.join(TOPDIR+localconfig.LOCALSITE+'/', 'preLoaderData/')
+datadir = os.path.join(TOPDIR,localconfig.LOCALSITE, 'preLoaderData/')
 
 
 ###############################
@@ -31,7 +32,7 @@ def load2cptloincmap(table):
     lines = getlines(datadir+table+'.txt')
    
     for l in lines:
-        id, cpt,cmpt,loinc =(x.strip() for x in l)
+        id, cpt,cmpt,loinc = [x.strip() for x in l]
         if id:
             try:
                 cl = CPTLOINCMap.objects.filter(id__exact=id)[0]
@@ -55,7 +56,7 @@ def load2ndc(table):
     lines = getlines(datadir+table+'.txt')
     
     for l in lines:
-        id, rulename, ndc,define,send =(x.strip() for x in l)
+        id, rulename, ndc,define,send = [x.strip() for x in l]
         
         r = Rule.objects.filter(ruleName__iexact=rulename)[0]
         #ndc = '0'+string.replace(ndc,'-','')
@@ -81,7 +82,7 @@ def load2ndc(table):
 def load2icd9(table):
     lines = getlines(datadir+table+'.txt')
     for l in lines:
-        id, rulename, icd,define,send = (x.strip() for x in l)
+        id, rulename, icd,define,send = [x.strip() for x in l]
         r = Rule.objects.filter(ruleName__iexact=rulename)[0]
         if string.strip(id):
             try:
@@ -105,7 +106,7 @@ def load2icd9(table):
 def load2loinc(table):
     lines = getlines(datadir+table+'.txt')
     for l in lines:
-        id, rulename, loinc,snmdposi,snmdnega,snmdinde,define,send = (x.strip() for x in l)
+        id, rulename, loinc,snmdposi,snmdnega,snmdinde,define,send = [x.strip() for x in l]
 
         r = Rule.objects.filter(ruleName__iexact=rulename)[0]
         if string.strip(id):
@@ -164,7 +165,7 @@ def  load2config(table):
     #print lines
   
     for items  in lines:
-        (id, t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25) = (x.strip() for x in items)
+        (id, t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25) = [x.strip() for x in items]
         if id:
             try:
                 cf = config.objects.filter(id__exact=id)[0]
@@ -202,7 +203,61 @@ def  load2config(table):
         cf.instIDcel= t24
         cf.instComments=t25      
         cf.save()
-   
+
+###################################
+###################################
+#################################
+def makecpt():
+    """found these at www.tricare.osd.mil/tai/downloads/cpt_codes.xls
+    """
+    reader = csv.reader(open('cpt_codes.csv','rb'),dialect='excel')
+    header = reader.next()
+    now = datetime.datetime.now().strftime("%Y-%m-%d")
+    i = 0
+    for ll in reader: # here be dragons. lots of "" at the 4th pos - but some other subtle crap too...
+        code,long,short = ll[:3] # good thing it doesn't really matter..
+        i += 1
+        if i % 10000 == 0:
+            print i,'cpt done'
+        long = long.replace('"','')
+        short = short.replace('"','')
+        c = cpt(cptCode=code,cptLong=long.capitalize(),cptShort=short.capitalize(),cptLastedit=now)
+        c.save()
+
+
+###################################
+def makeicd9():
+    """ found these codes somewhere or other..."""
+    codes = []
+    n = 1
+    from ESPicd9 import icd
+    for line in icd.split('\n'):
+        if n % 10000 == 0:
+            print n,'icd done'
+        n += 1
+        line = line.replace("'",'')
+        code,trans = line.split('\t')
+        code = '%s.%s' % (code[:3],code[3:]) # make all 3 digit decimals
+        c = icd9(icd9Code=code,icd9Long=trans.capitalize())
+        c.save()
+
+
+###################################
+def makendc():
+    """ found these codes somewhere http://www.fda.gov/cder/ndc/"""
+    f = file('ndc_codes.txt','r')
+    foo = f.next() # lose header
+    n = 1
+    for line in f:
+        if n % 10000 == 0:
+            print n,'ndc done'
+        n += 1
+        lbl = line[8:14]
+        prod = line[15:19]
+        trade = line[44:].strip()
+        newn = ndc(ndcLbl=lbl.capitalize(),ndcProd=prod.capitalize(),ndcTrade=trade.capitalize())
+        newn.save()
+                                                                                                                                                                                                                                            
             
 ################################
 ################################
@@ -211,9 +266,13 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         try:
             table = sys.argv[1]
-            if table == 'esp_conditionndc':
+            if table == 'remake':
+                makendc()
+                makeicd9() 
+                makecpt()
+            elif table == 'esp_conditionndc':
                 load2ndc(table)
-            if table == 'esp_conditionicd9':
+            elif table == 'esp_conditionicd9':
                 load2icd9(table)
             elif table == 'esp_conditionloinc':
                 load2loinc(table)
@@ -225,7 +284,7 @@ if __name__ == "__main__":
                 load2rule(table)
             else:
                 msg = 'Unknown table - %s\n' % table
-                log.write(msg)
+                logging.info(msg)
 
             logging.info('Start: %s\n' %  startt)
             logging.info('End:   %s\n' % datetime.datetime.now())
