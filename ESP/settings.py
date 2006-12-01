c316818f95ab9c71144c13a46b1638781bbf3036
rexua <rexua@localhost>
2006-12-01 18:23:17 +0000

    Set debug to false

--- ESP/settings.py
+++ ESP/settings.py
@@ -1,12 +1,12 @@
 # Django settings for esp project.
  
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 #ENABLE_PSYCO = True
 
-SITEROOT = ''
-TOPDIR='C:/django/'
-CODEDIR = TOPDIR+'ESP'
+SITEROOT = '/ESP'
+TOPDIR='/home/ESP'
+CODEDIR = '%s/ESP' % TOPDIR
 
 ADMINS = (
     ('Ross Lazarus', 'ross.lazarus@gmail.com'),
@@ -27,11 +27,10 @@ if usesqlite:
     DATABASE_PORT = 3306             # Set to empty string for default. Not used with sqlite3.
 else:
     DATABASE_ENGINE = 'mysql' # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-    DATABASE_NAME = 'espmr'             # Or path to database file if using sqlite3.
-    DATABASE_USER = 'rossSQL'             # Not used with sqlite3.
-    DATABASE_PASSWORD = 'SQLross'         # Not used with sqlite3.
-    DATABASE_HOST = 'godzilla'             # Set to empty string for localhost. Not used with sqlite3.
-    #DATABASE_HOST = '127.0.0.1'             # Set to empty string for localhost. Not used with sqlite3.
+    DATABASE_NAME = 'esp'             # Or path to database file if using sqlite3.
+    DATABASE_USER = 'ESP'             # Not used with sqlite3.
+    DATABASE_PASSWORD = '3spuser2006'         # Not used with sqlite3.
+    DATABASE_HOST = '127.0.0.1'             # Set to empty string for localhost. Not used with sqlite3.
     DATABASE_PORT = 3306             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. All choices can be found here:
@@ -51,12 +50,12 @@ MEDIA_ROOT = '%s/templates/' % CODEDIR
 
 # URL that handles the media served from MEDIA_ROOT.
 # Example: "http://media.lawrence.com"
-MEDIA_URL = ''
+MEDIA_URL = '/media'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/ESP/demo/media/' 
+ADMIN_MEDIA_PREFIX = '/ESP/media/' 
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'lo(g8i)a(g%&(4-*9@w#w2u#wfq(tjf5o+f4#6q7mg(k%9h%j5'
@@ -84,6 +83,8 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     "%s/templates" % CODEDIR,
     "%s/templates/esp" % CODEDIR,
+    "%s/templates" % TOPDIR,
+    "%s/templates/esp" % TOPDIR,
 )
 
 INSTALLED_APPS = (
