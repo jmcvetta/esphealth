ce3005c462822ce4a0432aaa4cce125fd1772dc1
jmcvetta <jmcvetta@localhost>
2009-01-27 22:20:56 +0000

    Backfill script now works.

--- src/ESP/backfill-by-month.sh
+++ src/ESP/backfill-by-month.sh
@@ -1,28 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Script to load old North Adams HL7 data one month at a time, to avoid
 # overwheling the import system.
 #
 #
-# Does NOT work yet!!!!
-#
 
 ESP_HOME=/home/rejmv/work/esp/
 ESP_SRC=$ESP_HOME/src/ESP/
-NORTH_ADAMS_DATA=$ESP_HOME/NORTH_ADAMS/
+ALL_HL7=$ESP_HOME/NORTH_ADAMS/all_HL7/
+INCOMING_HL7=$ESP_HOME/NORTH_ADAMS/incomingHL7/
+INCOMING_DATA=$ESP_HOME/NORTH_ADAMS/incomingData/
 
 
-cd $NORTH_ADAMS_DATA/all_hl7
-mv 2008-4* ../incomingHL7/
+echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo '2008-4'
+echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+cd $ALL_HL7
+cp 2008-4* $INCOMING_HL7
 cd $ESP_SRC
+rm $INCOMING_DATA/*
 python utils/hl7_to_etl.py
 python utils/incomingParser.py
 
 
-for filespec in _2008-{4..12}-; do
-	cd $NORTH_ADAMS_DATA/all_hl7
-	mv *$filespec* ../incomingHL7/
+for year_month in 2008-{4..12}; do
+	echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+	echo $year_month
+	echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+	cd $ALL_HL7
+	cp *_$year_month-* $INCOMING_HL7
 	cd $ESP_SRC
+	rm $INCOMING_DATA/*
 	python utils/hl7_to_etl.py
 	python utils/incomingParser.py
 done
