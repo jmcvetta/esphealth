f80044fd09fe7a42811bde2ab2ab607c315a0730
jmcvetta <jmcvetta@localhost>
2009-02-10 16:15:22 +0000

    Shell script to setup environment variables for ESP Django instance.

---
+++ src/ESP/setup_esp_env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#            Setup shell environment for use with an ESP component
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Usage:
+#   $ source ./esp_env.sh
+#
+# WARNING: Strange behavior may occur if you source this file from anywhere
+# except the folder where it lives.
+#
+
+echo $0 | grep -q esp_env 
+if [ $? -eq 0 ]; then
+    echo "Usage:" >> /dev/stderr
+    echo "  $ source ./setup_esp_env.sh" >> /dev/stderr
+    exit
+fi
+
+cur_dir=`pwd`
+mod_name=`basename $cur_dir`
+export PYTHONPATH=`cd ..; pwd`
+export DJANGO_SETTINGS_MODULE=$mod_name.settings
+echo Shell enviornment has been configured for $mod_name:
+echo "    PYTHONPATH=$PYTHONPATH"
+echo "    DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE"
