431a93bfd0202bc85cb3382000375783827a92cc
jmcvetta <jmcvetta@localhost>
2009-04-08 19:19:59 +0000

    This urls.py works for me, whereas the old one does not.
    Former-commit-id: 9b16f0a014930da973b23f635c625110a6d31e30

--- src/ESP/urls.py
+++ src/ESP/urls.py
@@ -6,35 +6,26 @@ from django.conf.urls.defaults import patterns
 from django.conf.urls.defaults import url
 from django.conf.urls.defaults import include
 from django.contrib.auth.views import login, logout
-
 from django.contrib import admin
 
-from ESP.esp.views import index
-
-from settings import MEDIA_ROOT, MEDIA_URL
+from ESP.settings import MEDIA_ROOT
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = patterns('',
+    # Django Admin
+    url(r'^ESP/admin/(.*)', admin.site.root, name='db_admin'),
+    url(r'^ESP/admin/doc/', include('django.contrib.admindocs.urls')),
     
     # Core Application
-    (r'^$', index),
-    (r'^esp/', include('ESP.esp.urls')),
-    
+    url(r'^ESP/', include('ESP.esp.urls')),
+    url(r'', include('ESP.esp.urls')),
+
     # Vaers
     (r'^vaers/', include('ESP.vaers.urls')),
     
-    
-    
-    # Django Admin
-    (r'^admin/(.*)', admin.site.root),
-#    (r'^admin/doc/', include('django.contrib.admindocs.urls'),
-    
-
-
     # Static Content 
     #
     # TODO: Remove before deployment into production!
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
 )
