0993bac189f870c711a948bdc1eb04f8c4ec1846
jmcvetta <jmcvetta@localhost>
2009-04-09 15:43:55 +0000

    various changes to make web UI work
    Former-commit-id: 8b4198edb0e7b2521353d41daa93105d0206d835

--- src/ESP/settings.py
+++ src/ESP/settings.py
@@ -86,7 +86,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
-    'esp.context_processors.path_definitions' 
+    'ESP.esp.context_processors.path_definitions' 
     )
 
 
