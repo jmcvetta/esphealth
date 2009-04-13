ca01bdab408200ec48188b0e95b514bb113aa5ca
jmcvetta <jmcvetta@localhost>
2009-04-13 21:34:15 +0000

    Maybe we don't want to remove deprecated fields just *quite* yet.
    Former-commit-id: ca99d7b8c3e2967ab02f1d7fd863ee7c96e44634

--- src/ESP/utils/incomingParser.py
+++ src/ESP/utils/incomingParser.py
@@ -410,9 +410,11 @@ def parseLxOrd(incomdir,filename,demogdict, provdict):
             LxMedical_Record_Number = mrn, 
             LxOrder_Id_Num=orderid,
             LxHVMA_Internal_Accession_number = accessnum,
+            LxTest_Code_CPT = cpt,
             LxOrderDate = orderd,
             LxOrderType = ordertp
             )
+        lx.LxTest_Code_CPT_mod = modi
         lx.LxLoinc = lxloinc
         lx.LxOrdering_Provider_id = provid
         lx.lastUpdate = DBTIMESTR
@@ -508,6 +510,9 @@ def parseLxRes(incomdir,filename,demogdict, provdict):
             LxMedical_Record_Number=mrn, 
             LxOrder_Id_Num=orderid,
             LxHVMA_Internal_Accession_number=accessnum,
+            LxTest_Code_CPT=cpt,
+            LxComponent=comp,
+            LxComponentName=compname,
             LxTest_results=res,
             LxOrderDate=orderd,
             LxOrderType=ordertp,
