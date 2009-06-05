f982bb92740aea0c8f7c21f3d875362b74dbbc01
Raphael Lullis <raphael@lullis.net>
2009-06-05 21:08:46 +0000

    reporting utility now writes file in a different folder (assets/reports)
    Former-commit-id: 9468786cfdd3fa194cbb48ee2ec5a51e3fdd0bc4

--- src/ESP/vaers/reports.py
+++ src/ESP/vaers/reports.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from ESP.vaers.models import EncounterEvent, LabResultEvent
+import os
 
-def write_temporal_clustering_reports():
-    EncounterEvent.write_fever_clustering_file_report()
-    EncounterEvent.write_diagnostics_clustering_file_report()
-    LabResultEvent.write_clustering_file_report()
+from ESP.vaers.models import EncounterEvent, LabResultEvent
+from ESP import settings
+
+REPORTS_DESTINATION_FOLDER = os.path.join(settings.TOPDIR, 'assets', 'reports')
+
+def write_temporal_clustering_reports(folder):
+    EncounterEvent.write_fever_clustering_file_report(folder)
+    EncounterEvent.write_diagnostics_clustering_file_report(folder)
+    LabResultEvent.write_clustering_file_report(folder)
 
 
 if __name__ == '__main__':
-    write_temporal_clustering_reports()
+    write_temporal_clustering_reports(REPORTS_DESTINATION_FOLDER)
 
 
 
