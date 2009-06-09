8f4b34f775c44dc88311f74fbe54833c9638c436
Jason McVetta <jason.mcvetta@gmail.com>
2009-06-09 18:45:28 +0000

    Choices file for HEF
    Former-commit-id: 5aceca4fcbb386a433f7c0963b40f97072c05525

---
+++ src/ESP/hef/choices.py
@@ -0,0 +1,18 @@
+'''
+                              ESP Health Project
+Heuristic Events Framework
+                            Choices for Data Models
+
+@authors: Jason McVetta <jason.mcvetta@gmail.com>
+@organization: Channing Laboratory http://www.channing.harvard.edu
+@copyright: (c) 2009 Channing Laboratory
+@license: LGPL
+'''
+
+
+HEF_RUN_STATUS = [
+    ('r', 'Run in progress'),
+    ('a', 'Aborted by user'),
+    ('s', 'Successfully completed'),
+    ('f', 'Failure'),
+    ]
