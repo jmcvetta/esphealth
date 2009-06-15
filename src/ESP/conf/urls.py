7e57822a9c6a4f53a69fa8f844ce345379e58f3e
Jason McVetta <jason.mcvetta@gmail.com>
2009-06-15 20:49:22 +0000

    Disable URL for not yet implemented view
    Former-commit-id: e371715222f66dee7c4d0857a3ba16f6ea02464f

--- src/ESP/conf/urls.py
+++ src/ESP/conf/urls.py
@@ -21,7 +21,7 @@ from ESP.esp.views import index, esplogin
 
 urlpatterns = patterns('ESP.conf.views',
     url(r'^codes/loinc/$', 'loinc_mapping'),
-    url(r'^codes/native/$', 'native_mapping'),
+    #url(r'^codes/native/$', 'native_mapping'),
     url(r'^codes/map/$', 'map_code'),
     url(r'^codes/map/loinc/(?P<loinc_num>\d+)/$', 'map_code', name='map_loinc'),
     url(r'^codes/map/native/(?P<native_code>\d+)/$', 'map_code', name='map_native'),
