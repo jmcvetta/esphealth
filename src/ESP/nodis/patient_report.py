be8b2fa5b6a4b812bb9d8630ab378a4f0d4b4c24
Jason McVetta <jason.mcvetta@gmail.com>
2009-07-06 15:20:08 +0000

    better formatting for patient report summary
    Former-commit-id: ffc114a1a623cf512bce03a6d58d9eacce567d75

--- src/ESP/nodis/patient_report.py
+++ src/ESP/nodis/patient_report.py
@@ -99,31 +99,28 @@ def main():
     parser.add_option('--phi', action='store_true', dest='phi', default=False, 
         help='Include PHI in report')
     (options, args) = parser.parse_args()
+    if options.summary:
+        values = {'mrn': 'MRN', 'name': 'NAME', 'condition': 'CONDITION', 'date': 'DATE', 'id': 'Patient #'}
+        print '-' * 80
+        if options.phi:
+            print '%(mrn)-14s    %(name)-25s    %(condition)-15s    %(date)-10s' % values
+        else:
+            print '%(id)-10s    %(condition)-15s    %(date)-10s' % values
+        print '-' * 80
     for identifier in args:
         if options.dbid:
             try:
                 patient = Patient.objects.get(pk=identifier)
             except Patient.DoesNotExist:
-                print >> sys.stderr
                 print >> sys.stderr, 'ERROR:  No patient found with database ID # %s.' % identifier
-                print >> sys.stderr
                 continue
         else: # Look up MRN
             try:
                 patient = Patient.objects.get(mrn=identifier)
             except Patient.DoesNotExist:
-                print >> sys.stderr
                 print >> sys.stderr, 'ERROR:  No patient found with MRN "%s".' % identifier
-                print >> sys.stderr
                 continue
         if options.summary:
-            values = {'mrn': 'MRN', 'name': 'NAME', 'condition': 'CONDITION', 'date': 'DATE', 'id': 'Patient #'}
-            print '-' * 80
-            if options.phi:
-                print '%(mrn)-14s    %(name)-25s    %(condition)-15s    %(date)-10s' % values
-            else:
-                print '%(id)-10s    %(condition)-15s    %(date)-10s' % values
-            print '-' * 80
             patient_summary(patient=patient, options=options)
         else:
             patient_report(patient=patient, options=options)
