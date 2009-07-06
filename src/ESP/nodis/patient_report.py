5d619acdb7a9b45ecac2228e7274ad065874e6b9
Jason McVetta <jason.mcvetta@gmail.com>
2009-07-06 14:56:58 +0000

    Add --summary option to patient_report.py
    Former-commit-id: 6ede7cd03bd7e881d3d69908cabdc34498c5302b

--- src/ESP/nodis/patient_report.py
+++ src/ESP/nodis/patient_report.py
@@ -66,7 +66,25 @@ def patient_report(patient, options):
     print
     print
     
-    
+
+def patient_summary(patient, options):
+    cases = patient.case_set.all()
+    for c in cases:
+        values = {'id': patient.pk, 'condition': c.condition, 'date': c.date}
+        if options.phi:
+            values['mrn'] = patient.mrn
+            values['name'] = patient.name
+            print '%(mrn)-14s    %(name)-25s    %(condition)-15s    %(date)-10s' % values
+        else:
+            print '%(id)-10s    %(condition)-15s    %(date)-10s' % values
+    if not cases:
+        values = {'id': patient.pk, 'condition': 'NONE', 'date': '-'}
+        if options.phi:
+            values['mrn'] = patient.mrn
+            values['name'] = patient.name
+            print '%(mrn)-14s    %(name)-25s    %(condition)-15s    %(date)-10s' % values
+        else:
+            print '%(id)-10s    %(condition)-15s    %(date)-10s' % values
 
 
 def main():
@@ -76,27 +94,39 @@ def main():
         help='Lookup patient by MRN (default)')
     parser.add_option('-d', action='store_true', dest='dbid', default=False, 
         help='Lookup patient by database ID number')
+    parser.add_option('-s', '--summary', action='store_true', dest='summary', 
+        default=False,  help='Print summary of cases for each patient')
     parser.add_option('--phi', action='store_true', dest='phi', default=False, 
         help='Include PHI in report')
     (options, args) = parser.parse_args()
     for identifier in args:
         if options.dbid:
-	        try:
-	            patient = Patient.objects.get(pk=identifier)
-	        except Patient.DoesNotExist:
-	            print >> sys.stderr
-	            print >> sys.stderr, 'ERROR:  No patient found with database ID # %s.' % identifier
-	            print >> sys.stderr
-	            continue
+            try:
+                patient = Patient.objects.get(pk=identifier)
+            except Patient.DoesNotExist:
+                print >> sys.stderr
+                print >> sys.stderr, 'ERROR:  No patient found with database ID # %s.' % identifier
+                print >> sys.stderr
+                continue
         else: # Look up MRN
-	        try:
-	            patient = Patient.objects.get(mrn=identifier)
-	        except Patient.DoesNotExist:
-	            print >> sys.stderr
-	            print >> sys.stderr, 'ERROR:  No patient found with MRN "%s".' % identifier
-	            print >> sys.stderr
-	            continue
-        patient_report(patient=patient, options=options)
+            try:
+                patient = Patient.objects.get(mrn=identifier)
+            except Patient.DoesNotExist:
+                print >> sys.stderr
+                print >> sys.stderr, 'ERROR:  No patient found with MRN "%s".' % identifier
+                print >> sys.stderr
+                continue
+        if options.summary:
+            values = {'mrn': 'MRN', 'name': 'NAME', 'condition': 'CONDITION', 'date': 'DATE', 'id': 'Patient #'}
+            print '-' * 80
+            if options.phi:
+                print '%(mrn)-14s    %(name)-25s    %(condition)-15s    %(date)-10s' % values
+            else:
+                print '%(id)-10s    %(condition)-15s    %(date)-10s' % values
+            print '-' * 80
+            patient_summary(patient=patient, options=options)
+        else:
+            patient_report(patient=patient, options=options)
 
 
 if __name__ == '__main__':
