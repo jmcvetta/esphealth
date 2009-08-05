901ca785e08d537367b92c2b543de9b9a590ac72
Jason McVetta <jason.mcvetta@gmail.com>
2009-08-05 21:40:30 +0000

    Configure EMR admin interface.
    Former-commit-id: ab25cecb4803c53ba180e46e677b112bce01c558

--- src/ESP/emr/admin.py
+++ src/ESP/emr/admin.py
@@ -17,24 +17,52 @@ from ESP.emr.models import Provider
 from ESP.emr.models import Patient
 from ESP.emr.models import LabResult
 from ESP.emr.models import Encounter
+from ESP.emr.models import Prescription
+from ESP.emr.models import Immunization
 
 
-class Provider_Admin(admin.ModelAdmin):
-    list_display = ['name',]
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ['provider_id_num', 'name', 'dept']
+    search_fields = ['provider_id_num','last_name']
 
-class Patient_Admin(admin.ModelAdmin):
-    list_display = ['name',]
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['patient_id_num', 'name', 'pcp', 'zip']
+    search_fields = ['patient_id_num', 'last_name']
 
-class Lab_Result_Admin(admin.ModelAdmin):
-    list_display = ['native_code', 'patient', 'provider',]
+class LabResultAdmin(admin.ModelAdmin):
+    list_display = ['order_num', 'native_name', 'patient', 'provider', 'result_string']
+    list_display_links = ['order_num']
+    search_fields = ['native_code', 'native_name', 'patient', 'provider', 'result_string']
+    ordering = ['-date']
 
-class Encounter_Admin(admin.ModelAdmin):
-    list_display = ['patient', 'provider']
+class EncounterAdmin(admin.ModelAdmin):
+    list_display = ['native_encounter_num', 'patient', 'provider', 'date']
+    list_display_links = ['native_encounter_num']
     raw_id_fields = ['patient', 'provider']
+    search_fields = ['native_encounter_num', 'patient__last_name', 'provider__last_name']
+    ordering = ['-date']
+
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ['order_num', 'date', 'patient', 'name', 'dose']
+    search_fields = ['order_num', 'name', 'patient__last_name']
+    ordering = ['-date']
+
+class ImmunizationAdmin(admin.ModelAdmin):
+    list_display = ['imm_id_num', 'date', 'patient', 'name']
+    search_fields = ['imm_id_num', 'name', 'patient__last_name']
+    ordering = ['-date']
+
+class ProvenanceAdmin(admin.ModelAdmin):
+    list_display = ['provenance_id', 'timestamp', 'source', 'status']
+    list_display_links = ['provenance_id', 'source']
+    ordering = ['-timestamp']
+    search_fields = ['source']
+    list_filter = ['status']
 
 
-admin.site.register(Provider, Provider_Admin)
-admin.site.register(Patient, Patient_Admin)
-admin.site.register(LabResult, Lab_Result_Admin)
-admin.site.register(Encounter, Encounter_Admin)
-admin.site.register(Provenance)
+admin.site.register(Provider, ProviderAdmin)
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(LabResult, LabResultAdmin)
+admin.site.register(Encounter, EncounterAdmin)
+admin.site.register(Prescription, PrescriptionAdmin)
+admin.site.register(Provenance, ProvenanceAdmin)
