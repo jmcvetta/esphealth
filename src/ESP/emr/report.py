695eec02a255e800ffcd2c0afd0bf93a72720698
Raphael Lullis <raphael@lullis.net>
2009-08-11 22:57:36 +0000

    Adding methods to create patient summary documents. Containing all information about one specific patient as a single json-file.
    Former-commit-id: ebdf8e80ba4f1309d491feb5a08ce884910a48a5

--- src/ESP/emr/report.py
+++ src/ESP/emr/report.py
@@ -1,6 +1,14 @@
+import os
+
 from ESP.emr.models import Patient
 
 
+DOC_FOLDER = os.path.join(os.path.abspath('.'), 'assets', 'documents')
+
 if __name__ == '__main__':
-    p = Patient.objects.order_by('?')[0]
-    print p.document_summary()
+    print 'Creating summary documents for %d patients' % Patient.objects.count()
+    for p in Patient.objects.all():
+        f = open(os.path.join(DOC_FOLDER, 'patient.%d.%s.json' % (p.pk, p.patient_id_num)), 'w')
+        f.write(p.document_summary())
+        f.close()
+        
