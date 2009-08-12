0b5d6e2ba92e184115f219054a4aa1038635a9cb
Raphael Lullis <raphael@lullis.net>
2009-08-12 23:52:38 +0000

    Separating patient documents into different folder for easier browsing.
    Former-commit-id: a564ad3f4b8c79f35f6a86cd26de5011e0a7aa06

--- src/ESP/emr/report.py
+++ src/ESP/emr/report.py
@@ -6,9 +6,28 @@ from ESP.emr.models import Patient
 DOC_FOLDER = os.path.join(os.path.abspath('.'), 'assets', 'documents')
 
 if __name__ == '__main__':
-    print 'Creating summary documents for %d patients' % Patient.objects.count()
-    for p in Patient.objects.order_by('id'):
-        f = open(os.path.join(DOC_FOLDER, 'patient.%d.%s.json' % (p.pk, p.patient_id_num)), 'w')
-        f.write(p.document_summary())
-        f.close()
+
+
+    patient_count = Patient.objects.count()
+    print 'Creating folders'
+
+    for i in xrange(patient_count/1000):
+        folder_name = '%06d' % (i*1000)
+        full_path = os.path.abspath(os.path.join(DOC_FOLDER, folder_name))
+        if not os.path.exists(full_path):
+            os.mkdir(full_path)
+                                  
+    
+
+    print 'Creating summary documents for %d patients' % patient_count
+    for patient in Patient.objects.order_by('id'):
+        try:
+            PATIENT_FOLDER = os.path.join(DOC_FOLDER, '%06d' % int((patient.pk/1000)*1000) )
+            f = open(os.path.join(PATIENT_FOLDER, 'patient.%06d.%s.json' % (patient.pk, patient.patient_id_num)), 'w')
+            f.write(patient.document_summary())
+            f.close()
+        except Exception, why:
+            import pdb
+            pdb.set_trace()
+            print 'Could not process information for patient %d. Reason: %s' % (patient.pk, why)
         
