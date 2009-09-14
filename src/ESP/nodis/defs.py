7259e39cee1d7d269de4a10798cb6b1a95e9347c
Jason McVetta <jason.mcvetta@gmail.com>
2009-09-14 18:14:01 +0000

    Transcribe old disease definitions to new Nodis API.
    Former-commit-id: 589230ce59ac0bd88c5635cfa1783ae91f230671

--- src/ESP/nodis/defs.py
+++ src/ESP/nodis/defs.py
@@ -10,21 +10,21 @@
 @license: LGPL
 '''
 
-import pprint
-
-from django.db import connection
-
-from ESP.settings import DEFAULT_REPORTABLE_ICD9S
+from ESP.emr.models import Patient
 from ESP.hef import events # Load events
+from ESP.hef.core import BaseHeuristic
 from ESP.hef.events import CHLAMYDIA_LOINCS
 from ESP.hef.events import GONORRHEA_LOINCS
-from ESP.hef.core import BaseHeuristic
 from ESP.nodis.core import ComplexEventPattern
 from ESP.nodis.core import Condition
-from ESP.emr.models import Patient
+from ESP.settings import DEFAULT_REPORTABLE_ICD9S
+from django.db import connection
+import pprint
 
 
-#-------------------------------------------------------------------------------
+
+
+#===============================================================================
 #
 # Chlamydia
 #
@@ -40,8 +40,7 @@ chlamydia_1 = ComplexEventPattern(
 
 chlamydia = Condition(
     name = 'chlamydia',
-    patterns = [chlamydia_1,],
-    match_window = 1, # Irrelevant -- single test is match
+    patterns = [(chlamydia_1, 1)],
     recur_after = 28, # New cases after 28 days
     icd9s = [
         '788.7',
@@ -77,7 +76,7 @@ chlamydia = Condition(
 
 
 
-#-------------------------------------------------------------------------------
+#===============================================================================
 #
 # Gonorrhea
 #
@@ -93,8 +92,7 @@ gonorrhea_1 = ComplexEventPattern(
 
 gonorrhea = Condition(
     name = 'gonorrhea',
-    patterns = [gonorrhea_1,],
-    match_window = 1, # Irrelevant -- single test is match
+    patterns = [(gonorrhea_1, 1)],
     recur_after = 28, # New cases after 28 days
     icd9s = [
         '788.7',
@@ -130,9 +128,135 @@ gonorrhea = Condition(
     )
 
 
-#-------------------------------------------------------------------------------
+#===============================================================================
 #
-# Hepatitis A/B/C
+# Acute Hepatitis A
+#
+#-------------------------------------------------------------------------------
+
+jaundice_or_blood_2x = ComplexEventPattern(
+    patterns = [
+        'jaundice', 
+        'alt_2x', 
+        'ast_2x',
+        ],
+    operator = 'or',
+    )
+
+hep_a_1 = ComplexEventPattern(
+    name = 'Acute Hepatitis A Definition 1',
+    patterns = [
+        jaundice_or_blood_2x, # (#1 or #2 or #3)
+        'hep_a_igm_pos',       # AND #4
+        ],
+    operator = 'and',
+    )
+ 
+acute_hep_a = Condition(
+    name = 'acute_hep_a',
+    patterns = [(hep_a_1, 30)],
+    recur_after = -1, # Never recur
+    icd9s = DEFAULT_REPORTABLE_ICD9S,
+    icd9_days_before = 14,
+    fever = True,
+    lab_loinc_nums = ['1742-6', '1920-8', '22314-9', '14212-5', '16128-1'],
+    lab_days_before = 30,
+    )
+
+
+
+#===============================================================================
+#
+# Hepatitis B
+#
+#-------------------------------------------------------------------------------
+
+
+'''
+Acute Hepatitis B definitions:
+1)  (jaundice OR alt_5x OR ast_5x) 
+    AND hep_b_igm_ab 
+    WITHIN 14 days
+2)  (jaundice OR alt_5x OR ast_5x) 
+    AND (total_bilirubin_high OR high_calc_bilirubin)
+    AND (hep_b_surface OR hep_b_viral_dna)
+    WITHIN 21 days
+    AND NOT (chronic_hep_b OR hep_b_surface OR hep_b_viral_dna) EVER IN PAST
+3)  hep_b_surface
+    AND NOT (hep_b_surface OR hep_b_viral_dna OR jaundice) EVER IN PAST
+'''
+
+
+jaundice_or_blood_5x = ComplexEventPattern(
+    patterns = ['jaundice', 'alt_5x', 'ast_5x',],
+    operator = 'or',
+    )
+    
+hep_b_1 = ComplexEventPattern(
+    name = 'Acute Hepatitis B Definition 1',
+    patterns = [jaundice_or_blood_5x, 'hep_b_igm_pos'],
+    operator = 'and',
+    )
+
+bilirubin = ComplexEventPattern(
+    patterns = ['total_bilirubin_high', 'high_calc_bilirubin'],
+    operator = 'or'
+    )
+
+hep_b_lab_pos = ComplexEventPattern(
+    patterns = ['hep_b_surface_pos', 'hep_b_viral_dna_pos'],
+    operator = 'or'
+    )
+
+hep_b_2 = ComplexEventPattern(
+    name = 'Acute Hepatitis B definition 2',
+    patterns = [jaundice_or_blood_5x, bilirubin, hep_b_lab_pos],
+    operator = 'and',
+    exclude = ['chronic_hep_b'],
+    exclude_past = ['chronic_hep_b', 'hep_b_surface_pos', 'hep_b_viral_dna_pos'],
+    )
+
+hep_b_3 = ComplexEventPattern(
+    name = 'Hepatitis B definition 3',
+    patterns = ['hep_b_surface_pos'],
+    operator = 'and', # Meaningless w/ only one pattern
+    require_past = ['hep_b_surface_neg'],
+    require_past_window = 365, # 1 year
+    exclude = ['chronic_hep_b'],
+    exclude_past = ['chronic_hep_b', 'hep_b_surface_pos', 'hep_b_viral_dna_pos'],
+    )
+
+hep_b = Condition(
+    name = 'acute_hep_b',
+    patterns = [
+        (hep_b_1, 14),
+        (hep_b_2, 21),
+        (hep_b_3, 1), # Single present event -- match window is meaningless
+    ],
+    recur_after = -1, # Never recur
+    icd9s = DEFAULT_REPORTABLE_ICD9S,
+    icd9_days_before = 14,
+    fever = True,
+    lab_loinc_nums = [
+        '1742-6', 
+        '1920-8', 
+        '31204-1', 
+        '5195-3',
+        '13954-3',
+        '13126-8',
+        '5009-6',
+        '16943-2',
+        '14212-5',
+        '22314-9',
+        '16128-1',
+        ],
+    lab_days_before = 14,
+    )
+
+
+#===============================================================================
+#
+# Acute Hepatitis C
 #
 #-------------------------------------------------------------------------------
 
@@ -188,8 +312,7 @@ hep_c_1 = ComplexEventPattern(
 
 hep_c = Condition(
     name = 'acute_hep_c',
-    patterns = [hep_c_1, ],
-    match_window = 28, # days
+    patterns = [(hep_c_1, 28)],
     recur_after = -1, # Never recur
     icd9s = DEFAULT_REPORTABLE_ICD9S,
     icd9_days_before = 14,
@@ -213,3 +336,87 @@ hep_c = Condition(
     )
 
 
+
+#===============================================================================
+#
+# Lyme Disease
+#
+#-------------------------------------------------------------------------------
+
+
+lyme_test_pos = ComplexEventPattern(
+    patterns = ['lyme_elisa_pos', 'lyme_igg_pos', 'lyme_igm_pos', 'lyme_pcr_pos'],
+    operator = 'or'
+    )
+
+lyme_diag_ab = ComplexEventPattern(
+    patterns = ['lyme_diagnosis', 'doxycycline', 'lyme_other_antibiotics'],
+    operator = 'or'
+    )
+
+doxy_lyme_ab = ComplexEventPattern(
+    patterns = ['doxycycline', 'lyme_other_antibiotics'],
+    operator = 'or'
+    )
+
+lyme_1 = ComplexEventPattern(
+    name = 'Lyme Disease definition 1',
+    patterns = [lyme_test_pos, lyme_diag_ab],
+    operator = 'and',
+    )
+
+lyme_2 = ComplexEventPattern(
+    name = 'Lyme Disease definition 2',
+    patterns = ['lyme_diagnosis', doxy_lyme_ab],
+    operator = 'and'
+    )
+
+lyme_3 = ComplexEventPattern(
+    name = 'Lyme Disease Definition 3',
+    patterns = ['rash', 'lyme_elisa_ordered', 'doxycycline'],
+    operator = 'and'
+    )
+
+
+lyme = Condition(
+    name = 'lyme',
+    patterns = [
+        (lyme_1, 30),
+        (lyme_2, 14),
+        (lyme_3, 14),
+        ],
+    recur_after = 365, # 1 year
+    icd9s = [
+        '782.1',
+        '711.8',
+        '320.7',
+        '351.0',
+        '723.4',
+        '724.4',
+        '422.0',
+        '426.0',
+        '426.12',
+        '426.13',
+        '780.6',
+        ],
+    icd9_days_before = 30,
+    fever = True,
+    lab_loinc_nums = [
+        '5061-7',
+        '31155-5',
+        '16481-4',
+        '16482-2',
+        '23982-2',
+        '29898-4',
+        '4991-6',
+        ],
+    lab_days_before = 30,
+    med_names = [
+        'doxycycline',
+        'amoxicillin',
+        'cefuroxime',
+        'ceftriaxone',
+        'cefotaxime',
+        ],
+    med_days_before = 30,
+    )
