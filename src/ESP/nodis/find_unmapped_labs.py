ccc07a430c5f046a288a205e08b09a37ac560f78
Jason McVetta <jason.mcvetta@gmail.com>
2009-10-05 22:01:14 +0000

    More components of unmapped lab tests report
    Former-commit-id: bb1f11758cf9d05b792855d2fb1f8770b35d0942

---
+++ src/ESP/nodis/find_unmapped_labs.py
@@ -0,0 +1,36 @@
+'''
+                                  ESP Health
+                         Notifiable Diseases Framework
+                         Populate Unmapped Labs Cache
+
+
+@author: Jason McVetta <jason.mcvetta@gmail.com>
+@organization: Channing Laboratory <http://www.channing.harvard.edu>
+@copyright: (c) 2009 Channing Laboratory
+@license: LGPL 3.0 <http://www.gnu.org/licenses/lgpl-3.0.txt>
+'''
+
+from ESP.utils.utils import log
+from ESP.nodis.core import Condition
+from ESP.nodis.models import UnmappedLab
+from ESP.nodis import defs
+
+
+def main():
+    log.info('Purging and repopulating unmapped labs cache')
+    # Clear the cache
+    UnmappedLab.objects.all().delete()
+    log.debug('Unmapped labs cache purged')
+    # Populate the cache
+    for item in Condition.find_unmapped_labs():
+        ul = UnmappedLab()
+        ul.native_code = item['native_code']
+        ul.native_name = item['native_name']
+        ul.save()
+        log.debug('Added %s to unmapped labs cache' % item)
+    count = UnmappedLab.objects.all().count()
+    log.info('Populated unmapped labs cache with %s items' % count)
+    
+
+if __name__ == '__main__':
+    main()
