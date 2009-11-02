f1a4157725c6ac08383cbd90a6d8ec2bd2376c7b
Raphael Lullis <raphael@lullis.net>
2009-11-02 19:44:11 +0000

    Making sure that file writer can use line-separators in Windows style.
    Former-commit-id: 1e6e7b8c73602530fd613c0355cf9b193dfe74c7

--- src/ESP/vaers/utils.py
+++ src/ESP/vaers/utils.py
@@ -3,14 +3,14 @@ import datetime
 from django.contrib.sites.models import Site
 from django.db.models import Q
 
-def make_clustering_event_report_file(filename, events):
+def make_clustering_event_report_file(filename, events, newline_separator='\r\n'):
     f = open(filename, 'w')
     f.write('\t'.join(['id', 'vdate', 'edate', 'gap', 'vaccine', 
                        'comment', 'age', 'gender']))
-    f.write('\n')
+    f.write(newline_separator)
     for ev in events:
         f.write(ev.render_temporal_report())
-        f.write('\n')
+        f.write(newline_separator)
     f.close()
 
 
