546e699618afe9fb6194501a4ed1149bd3681889
Ross Lazarus <ross.lazarus@gmail.com>
2009-11-28 00:40:56 +0000

    doc and cleanup for run_ss_november.sh

--- ESP/run_ss_november.sh
+++ ESP/run_ss_november.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
+# espSS modified nov 28 to only count Urgent care and visit events - less than half of
+# all since denominators and rates are inflated
+# script to rerun all SS data as requested by Inna
+# one big file to oct03 and then daily files
+#
 echo 'remember to source /srv/esp/mysql-version/ESP/env-setup.sh first'
 mkdir -p assets/SS/test
-#python utils/espSS.py -s 20091031 -e 20091031 -z5 -o ./assets/SS/test -t -v 
-#python utils/espSS.py -s 20060701 -e 20091003 -z5 -o ./assets/SS/test -t -v 
-# date --date '54 days ago' +%Y%m%d
+python utils/espSS.py -s 20060701 -e 20091003 -z5 -o ./assets/SS/test -t -v 
+# date --date '54 days ago' +%Y%m%d takes us back to oct4
 for i in $(seq 0 1 54)
 do
 sd=`date --date "$i days ago" +%Y%m%d`
