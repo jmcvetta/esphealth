fa6567c34b077bc7af5252995a742f52b8d011d4
Raphael Lullis <raphael@lullis.net>
2009-11-30 22:10:34 +0000

    Moving ss scripts to scripts folder.

--- ESP/run_ss.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-mkdir -p assets/SS/2009/09
-mkdir -p assets/SS/2009/09/01
-mkdir -p assets/SS/2009/09/02
-
-python utils/espSS.py -s 20090901 -e 20090901 -z5 -o ./assets/SS/2009/09/01 -t -v 
-python utils/espSS.py -s 20090902 -e 20090902 -z5 -o ./assets/SS/2009/09/02 -t -v 
