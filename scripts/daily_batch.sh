cc70b536045fcd87bb833afdc6f2e99b0f1c4318
Raphael Lullis <raphael@lullis.net>
2009-11-30 22:11:11 +0000

    Changes in daily_batch.sh, to make it work with cron.

--- scripts/daily_batch.sh
+++ scripts/daily_batch.sh
@@ -7,15 +7,16 @@
 #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+BASEDIR=`dirname $0`
+ESP_HOME=$BASEDIR/../ESP
 
-ESP_HOME=`pwd`/../ESP
-
-export PYTHONPATH=`pwd`/..
+export PYTHONPATH=$BASEDIR/..
 export DJANGO_SETTINGS_MODULE=`basename $ESP_HOME`.settings
 
+python $ESP_HOME/utils/download_epic_files.py &&
+python $ESP_HOME/utils/incomingParser.py &&
+python $ESP_HOME/utils/identifyCases.py &&
+python $ESP_HOME/utils/send_hl7.py &&
 
-python $ESP_HOME/utils/download_epic_files.py
-python $ESP_HOME/utils/incomingParser.py
-source ./run_ss.sh
-source ./upload_ss_reports.sh
-
+# Sync to postgresql.
+/opt/pdi-3.1.0/kitchen.sh -rep pdi_3_1_0 -user esp -dir prod-sync -job prod-sync
