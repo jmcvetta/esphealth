c2c96c5e40e924bbf48e49924d1ed9b7f9db570d
Jason McVetta <jason.mcvetta@gmail.com>
2009-12-03 16:10:53 +0000

    Further progress on port of HL7 generator to new codebase.
    Former-commit-id: d7b0646ceb414dae1f1c47bbaf1c752a11515c1b

--- src/ESP/nodis/mdph_xml.py
+++ src/ESP/nodis/mdph_xml.py
@@ -1,11 +1,11 @@
 '''
                                   ESP Health
                          Notifiable Diseases Framework
-MDPH XML Generator
+                              MDPH XML Generator
 
 
 @author: Jason McVetta <jason.mcvetta@gmail.com>
-@organization: Channing Laboratory http://www.channing.harvard.edu
+@organization: Channing Laboratory - http://www.channing.harvard.edu
 @copyright: (c) 2009 Channing Laboratory
 @license: LGPL - http://www.gnu.org/licenses/lgpl-3.0.txt
 '''
@@ -20,6 +20,8 @@ VERSION = '2.3.1'
 import time
 from lxml import etree
 
+from ESP.emr.models import Provider
+from ESP.emr.models import Patient
 from ESP.nodis.models import Case
 
 
@@ -27,6 +29,55 @@ from ESP.nodis.models import Case
 # This is used in various places, and needs to be same everywhere
 #
 ISO_DATE = time.strftime('%Y%m%d%H%M%S',time.localtime())
+
+
+HL7_RACES = {
+    'CAUCASIAN':'W',
+    'BLACK':'B',
+    'OTHER':'O',
+    'HISPANIC': 'W',
+    'INDIAN':'I',
+    'ASIAN':'A',
+    'NAT AMERICAN':'I',
+    'NATIVE HAWAI':'P',
+    'ALASKAN':'I',
+    # '': 'U' # Removed because we handle null race = U in the code
+    }
+
+
+def make_name(element, person):
+    '''
+    Returns an Element containing name information.  Element is somewhat 
+    different when person is a patient than when person is a provider.
+    @param element: Name of the element to be created
+    @type element:  String
+    @param person: The person described by this element
+    @type person:  Patient or Provider object
+    '''
+    e = etree.Element(element)
+    if person.first_name:
+        first = person.first_name
+    else:
+        first = 'Unknown'
+    if person.last_name:
+        last = person.last_name
+    else:
+        last = 'Unknown'
+    if isinstance(person, Patient):
+        etree.SubElement(etree.SubElement(e, 'XPN.1'), 'FN.1').text = person.last_name
+#            worklist = [(firstName,'XPN.2'),(middleInit,'XPN.3'),(suffix,'XPN.4')]
+        etree.SubElement(e, 'XPN.2').text = person.first_name
+        etree.SubElement(e, 'XPN.3').text = person.middle_name
+        etree.SubElement(e, 'XPN.4').text = person.suffix
+    elif isinstance(person, Provider):
+        etree.SubElement(etree.SubElement(e, 'XCN.2'), 'FN.1').text = person.last_name
+#            worklist = [(firstName,'XPN.2'),(middleInit,'XPN.3'),(suffix,'XPN.4')]
+        etree.SubElement(e, 'XCN.3').text = person.first_name
+        etree.SubElement(e, 'XCN.4').text = person.middle_name
+        etree.SubElement(e, 'XCN.5').text = person.suffix
+    else:
+        raise RuntimeError('Invalid object passed as person')
+    return e
 
 
 def compose():
@@ -69,6 +120,8 @@ def encode_case(case):
     @param case: Case to be encoded in HL7v3
     @type case: Case
     '''
+    patient = case.patient
+    provider = case.provider
     oru = etree.Element('ORU_R01')
     #
     # Message Header
@@ -95,8 +148,38 @@ def encode_case(case):
     #
     #
     #
-    etree.SubElement(oru, 'ORU_R01.PIDPD1NK1NTEPV1PV2ORCOBRNTEOBXNTECTI_SUPPGRP')
+    oru_sub1 = etree.SubElement(oru, 'ORU_R01.PIDPD1NK1NTEPV1PV2ORCOBRNTEOBXNTECTI_SUPPGRP')
+    oru_sub2 = etree.SubElement(oru_sub1, 'ORU_R01.PIDPD1NK1NTEPV1PV2_SUPPGRP')
+    #
+    # Patient Information
+    #
+    pid = etree.SubElement(oru_sub2, 'PID')
+    etree.SubElement(pid, 'PID.1').text = '1'
+    # MRN
+    pid3 = etree.SubElement(pid, 'PID.3')
+    etree.SubElement(pid3, 'CX.1').text = patient.mrn
+    etree.SubElement(pid3, 'CX.5').text = 'MR'
+    etree.SubElement(etree.SubElement(pid3, 'CX.6'), 'HD.2').text = provider.dept
+    # SSN
+    pid3 = etree.SubElement(pid, 'PID.3') # New 'PID.3' section
+    etree.SubElement(pid3, 'CX.1').text = patient.ssn[-4:]
+    etree.SubElement(pid3, 'CX.5').text = 'SS'
+    # Name
+    pid.append( make_name('PID.5', patient) )
+    # DoB  -- do we need to convert date to some specific string format?
+    etree.SubElement(etree.SubElement(pid, 'PID.7'), 'TS.1').text = '%s' % patient.date_of_birth
+    # Gender
+    etree.SubElement(pid, 'PID.8').text = patient.gender
+    # Race
+    try:
+        race = HL7_RACES[patient.race.upper()]
+    except:
+        race = 'U'
+    etree.SubElement(etree.SubElement(pid, 'PID.10'), 'CE.4').text = race
+    # Home Address
     return oru
+
+
     
 
 
