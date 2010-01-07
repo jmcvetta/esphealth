ea03062a8085be2d915fb4e31c5c9509727c2d35
Jason McVetta <jason.mcvetta@gmail.com>
2010-01-07 18:09:28 +0000

    Order by PK when checking floats
    Former-commit-id: e71f851958bbc7f33eea24f3d1105a72b99d6c83

--- src/ESP/emr/etl/float_checker.py
+++ src/ESP/emr/etl/float_checker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 '''
-Rolls through all EMR records, checking that relevant string fields have been correctly converted to float fields. 
+Rolls through all EMR records, checking that relevant string fields have been
+correctly converted to float fields. 
 '''
 
 import re
@@ -8,33 +9,10 @@ import sys
 import pprint
 import datetime
 
-from django.db import connection
 from django.db import transaction
 from django.db.models import Q
-from django.db.models import F
-from django.db.models import Sum
-from django.db.models import Count
-from ESP.emr.models import Patient
 from ESP.emr.models import LabResult
 from ESP.emr.models import Prescription
-
-from ESP.nodis.models import ComplexEventPattern
-from ESP.nodis.models import SimpleEventPattern
-from ESP.nodis.models import Condition
-from ESP.nodis import defs
-
-from ESP.nodis.defs import jaundice_alt400
-from ESP.nodis.defs import no_hep_b_surf
-from ESP.nodis.defs import no_hep_b
-from ESP.nodis.defs import hep_c_1
-from ESP.nodis.defs import hep_c
-from ESP.nodis.defs import chlamydia
-
-from ESP.nodis.models import Case
-from ESP.nodis.models import Pattern
-from ESP.hef.models import Event
-
-from ESP.conf.models import CodeMap
 from ESP.utils.utils import log_query
 
 
@@ -53,7 +31,7 @@ def float_or_none(self, string):
 
 def check_prescriptions():
     i = 0
-    qs = Prescription.objects.filter(quantity__isnull=False, quantity_float__isnull=True)
+    qs = Prescription.objects.filter(quantity__isnull=False, quantity_float__isnull=True).order_by('pk')
     tot = qs.count()
     for rx in qs:
         i += 1
@@ -74,7 +52,7 @@ def check_labs():
     q_obj = Q(ref_high_string__isnull=False, ref_high_float__isnull=True)
     q_obj |= Q(ref_low_string__isnull=False, ref_low_float__isnull=True)
     q_obj |= Q(result_string__isnull=False, result_float__isnull=True)
-    qs = LabResult.objects.filter(q_obj)
+    qs = LabResult.objects.filter(q_obj).order_by('pk')
     log_query('Lab results to check for float results', qs)
     tot = qs.count()
     for lab in qs:
@@ -93,9 +71,7 @@ def check_labs():
         print 'Lab  %20s/%s' % (i, tot)
 
 
-def main():
-    check_prescriptions()
-    check_labs()
-
 if __name__ == '__main__':
     check_labs()
+    check_prescriptions()
+    
