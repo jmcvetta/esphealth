6cdfe1594ef8d9d75ed852f1dd8f44dcc1418262
Jason McVetta <jason.mcvetta@gmail.com>
2010-01-15 19:30:26 +0000

    Refactor hef/run.py to work as "manage.py hef" command.
    Former-commit-id: bf3820a5fc8c44acb11864135fdba37b870cdc45

---
+++ src/ESP/hef/management/commands/hef.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+'''
+                                  ESP Health
+                          Heuristic Events Framework
+                              Command Line Runner
+
+
+@author: Jason McVetta <jason.mcvetta@gmail.com>
+@organization: Channing Laboratory http://www.channing.harvard.edu
+@cooptions['args']:pyright: (c) 2009 Channing Laboratory
+
+@license: LGPL
+'''
+
+import sys
+import datetime
+import optparse
+
+from django.db import connection
+from django.core.management.base import BaseCommand
+from optparse import make_option
+
+
+from ESP.hef.core import BaseHeuristic
+from ESP.hef.models import Run
+from ESP.hef.events import * # Load all Event definitions
+from ESP import settings
+from ESP.utils.utils import log
+
+
+
+
+#===============================================================================
+#
+#--- ~~~ Main ~~~
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    
+class Command(BaseCommand):
+    
+    help = 'Generate heuristic events'
+    
+    args = '[HEURISTIC, HEURISTIC, ...]'
+    usage_str = '[options] [NAME, NAME, ...] \n'
+    usage_str += '\n'
+    usage_str += 'Runs heuristic NAME if specified; otherwise, runs all heuristics.\n'
+    usage_str += '\n'
+    option_list = BaseCommand.option_list + (
+        make_option('--list', action='store_true', dest='list', 
+            help='List names of all registered heuristics'),
+        )
+
+    def handle(self, *args, **options):
+        # 
+        # TODO: We need a lockfile or some othermeans to prevent multiple 
+        # instances running at once.
+        #
+        log.debug('options: %s' % options)
+        # 
+        # Main
+        #
+        if options['list']:
+            h_names =  [h.name for h in BaseHeuristic.all_heuristics()]
+            h_names.sort()
+            for name in h_names:
+                print name
+            sys.exit()
+        this_run = Run()
+        this_run.save()
+        if args:
+            for name in args:
+                try:
+                    BaseHeuristic.generate_events_by_name(name=name, run=this_run)
+                except KeyError:
+                    print >> sys.stderr, 'Unknown heuristic name: "%s"' % name
+        else:
+            BaseHeuristic.generate_all_events(run=this_run)
+        
