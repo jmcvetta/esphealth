d99fe147f3b0df0287c58becafb3886d8d0ae597
Jason McVetta <jason.mcvetta@gmail.com>
2010-01-15 19:54:52 +0000

    Refactor nodis/run.py to work as "manage.py nodis" command.
    Former-commit-id: bed4a8b5fb080f9ac96ef64548d01b8fe542f78e

--- src/ESP/nodis/management/commands/nodis.py
+++ src/ESP/nodis/management/commands/nodis.py
@@ -17,6 +17,8 @@ import optparse
 import pprint
 
 from django.db import connection
+from django.core.management.base import BaseCommand
+from optparse import make_option
 
 from ESP import settings
 from ESP.utils import utils as util
@@ -25,50 +27,40 @@ from ESP.nodis.models import Condition
 from ESP.nodis import defs  # Register definitions
 
     
-USAGE_MSG = '''\
-%prog [--regenerate] [conditions] 
+class Command(BaseCommand):
+    
+    help = 'Generate Nodis cases'
+    
+    args = '[conditions]'
+    
+    option_list = BaseCommand.option_list + (
+        make_option('--regenerate', action='store_true', dest='regenerate', 
+            help='Purge and regenerate cases'),
+        )
+    # TODO: Add --list argument
 
-Where conditions are one or more of the following:
-'''
-USAGE_MSG += '\n'.join(['    %s' % c.name for c in Condition.all_conditions()])
-
-
-def main():
-    # 
-    # TODO: We need a lockfile or some othermeans to prevent multiple 
-    # instances running at once.
-    #
-    parser = optparse.OptionParser(usage=USAGE_MSG)
-    parser.add_option('--regenerate', action='store_true', dest='regenerate', 
-        help='Purge and regenerate cases')
-    (options, args) = parser.parse_args()
-    log.debug('options: %s' % options)
-    #
-    # Main Control block
-    #
-    all_condition_names = [c.name for c in Condition.all_conditions()]
-    if args: 
-        conditions = set()
-        for name in args:
-            if name not in all_condition_names:
-                sys.stderr.write("Unknown condition: '%s'.  Exiting.\n" % name)
-            con = Condition.get_condition(name)
-            assert con, 'Unknown condition: %s' % name
-            conditions.add(con)
-    else:
-        conditions = Condition.all_conditions()
-    #
-    # (Re)generate Cases
-    #
-    for c in conditions:
-        if options.regenerate:
-            c.regenerate()
+    def handle(self, *args, **options):
+        # 
+        # TODO: We need a lockfile or some othermeans to prevent multiple 
+        # instances running at once.
+        #
+        all_condition_names = [c.name for c in Condition.all_conditions()]
+        if args: 
+            conditions = set()
+            for name in args:
+                if name not in all_condition_names:
+                    sys.stderr.write("Unknown condition: '%s'.  Exiting.\n" % name)
+                con = Condition.get_condition(name)
+                assert con, 'Unknown condition: %s' % name
+                conditions.add(con)
         else:
-            c.generate_cases()
-
-
-if __name__ == '__main__':
-    main()
-    if settings.DEBUG:
-        # Query count is only retained in DEBUG mode
-        print 'Total Number of DB Queries: %s' % len(connection.queries)
+            conditions = Condition.all_conditions()
+        #
+        # (Re)generate Cases
+        #
+        for c in conditions:
+            if options['regenerate']:
+                c.regenerate()
+            else:
+                c.generate_cases()
+    
