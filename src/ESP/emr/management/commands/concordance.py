c4a47f8ba7c1d2db0f8ac9acea09113c1cc65f3b
Jason McVetta <jason.mcvetta@gmail.com>
2010-01-19 15:17:59 +0000

    Refactor for new "manage.py concordance" command.
    Former-commit-id: 8cacb0f98ffd74ddaa18a8f711f93cefac61606c

--- src/ESP/emr/management/commands/concordance.py
+++ src/ESP/emr/management/commands/concordance.py
@@ -13,6 +13,8 @@
 from django.db import transaction
 from django.db.models import Q
 from django.db.models import Count
+from django.core.management.base import BaseCommand
+from optparse import make_option
 
 from ESP.utils.utils import log
 from ESP.utils.utils import log_query
@@ -21,36 +23,37 @@ from ESP.emr.models import LabTestConcordance
 
 
 
-@transaction.commit_manually
-def main():
-    log.info('Repopulating lab test concordance')
-    log.debug('Begin transaction')
-    # Clear the existing table
-    LabTestConcordance.objects.all().delete()
-    log.debug('Purged concordance table.')
-    # Generate the concordance -- *LONG* query
-    qs = LabResult.objects.all().values('native_code', 'native_name').distinct()
-    qs = qs.annotate(count=Count('id'))
-    log_query('Concordance query', qs)
-    for item in qs:
-        l = LabTestConcordance()
-        l.native_code = item['native_code']
-        l.native_name = item['native_name']
-        l.count = item['count']
-        l.save()
-        transaction.commit()
-        log.debug('Added %s to concordance' % item)
-        try:
+class Command(BaseCommand):
+    
+    help = 'Generate concordance of lab tests.'
+    
+    @transaction.commit_manually
+    def handle(self, *fixture_labels, **options):
+        log.info('Repopulating lab test concordance')
+        log.debug('Begin transaction')
+        # Clear the existing table
+        LabTestConcordance.objects.all().delete()
+        log.debug('Purged concordance table.')
+        # Generate the concordance -- *LONG* query
+        qs = LabResult.objects.all().values('native_code', 'native_name').distinct()
+        qs = qs.annotate(count=Count('id'))
+        log_query('Concordance query', qs)
+        for item in qs:
+            l = LabTestConcordance()
+            l.native_code = item['native_code']
+            l.native_name = item['native_name']
+            l.count = item['count']
             l.save()
             transaction.commit()
             log.debug('Added %s to concordance' % item)
-        except:
-            log.error('Could not save to concordance.')
-            transaction.rollback()
-    count = LabTestConcordance.objects.all().count()
-    log.debug('Transaction committed')
-    log.info('Populated lab test concordance with %s items' % count)
-    
-
-if __name__ == '__main__':
-    main()
+            try:
+                l.save()
+                transaction.commit()
+                log.debug('Added %s to concordance' % item)
+            except:
+                log.error('Could not save to concordance.')
+                transaction.rollback()
+        count = LabTestConcordance.objects.all().count()
+        log.debug('Transaction committed')
+        log.info('Populated lab test concordance with %s items' % count)
+        
