d4823531d717a2e2c01f56e28db82f299d3303d4
Jason McVetta <jason.mcvetta@gmail.com>
2010-01-29 18:52:27 +0000

    Remove aggregate stats from concordance -- not necessary at this time.
    Former-commit-id: 94d8a4bed20fb61b765f199c9ca02c9f02b4d1f4

--- src/ESP/emr/models.py
+++ src/ESP/emr/models.py
@@ -102,10 +102,6 @@ class LabTestConcordance(models.Model):
     native_code = models.CharField(max_length=100, blank=False)
     native_name = models.CharField(max_length=255, null=True)
     count = models.IntegerField(blank=False)
-    min_ref_low = models.FloatField(blank=True, null=True)
-    max_ref_high = models.FloatField(blank=True, null=True)
-    min_result = models.FloatField(blank=True, null=True)
-    max_result = models.FloatField(blank=True, null=True)
 
 
 
