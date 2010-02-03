8c377ea9cf9c896568da323c2a8502820f69a811
Raphael Lullis <raphael@lullis.net>
2010-02-03 20:18:31 +0000

    Corrections to espmanager.doFTP
    Former-commit-id: deddb2a7391c00bb46a1ac8f15624ee90a58d1ff

--- src/ESP/utils/espmanager.py
+++ src/ESP/utils/espmanager.py
@@ -1,11 +1,11 @@
-import os
+import os, sys
 import datetime
 
 from optparse import OptionParser
 from ftplib import FTP
 
 from ESP.settings import DATA_DIR, FTP_USER, FTP_PASSWORD, FTP_SERVER
-from ESP.utils.utils import log, days_in_interval, str_from_date
+from ESP.utils.utils import log, days_in_interval, str_from_date, date_from_str
 
 usage_msg = """espmanager.py
 Usage: python utils/espmanager.py --begin=[date as 20090101] --end_date=[date] 
@@ -20,26 +20,30 @@ def doFTP(days):
         return
         
     #local site directory
-    todir = os.path.join(DATA_DIR, 'incoming/')
+    todir = os.path.join(DATA_DIR, 'epic', 'incoming')
     os.chdir(todir)    
     #remote site directory
     ftp.cwd('ESPFiles')
     filenames = ftp.nlst()
            
-    
-    datestamps = [str_from_date(day) for day in days]
+    datestamps = [day.strftime('%m%d%Y') for day in days]
 
     for eachfile in filenames:
         if eachfile.split('.')[-1] in datestamps:
+            log.info('Retrieving file ' + eachfile)
             ftp.retrbinary('RETR ' + eachfile, open(eachfile, 'wb').write)
             
     ftp.quit()
-    return newfiles
+    return
 
 
 if __name__ == "__main__":
     today = datetime.datetime.today()
     yesterday = today - datetime.timedelta(1)
+
+    parser = OptionParser(usage=usage_msg)
+    parser.add_option('-b', '--begin', dest='begin_date', default=yesterday.strftime('%Y%m%d'))
+    parser.add_option('-e', '--end', dest='end_date', default=today.strftime('%Y%m%d'))
 
     options, args = parser.parse_args()
 
