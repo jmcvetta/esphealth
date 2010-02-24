d99f27a79eb8e93ff994a0757aedd4a40ebcc3c1
Jason McVetta <jason.mcvetta@gmail.com>
2010-02-24 21:31:41 +0000

    Add index on hef_event (patient_id, name, date);
    Former-commit-id: bea5457632b7daa7a649166a57ab01aa726ebea1

--- src/ESP/emr/sql/multicolumn-indices.postgresql.sql
+++ src/ESP/emr/sql/multicolumn-indices.postgresql.sql
@@ -6,6 +6,7 @@
 -- These indices provide significant performance improvements for our queries.
 --------------------------------------------------------------------------------
 CREATE INDEX hef_event_name_date ON hef_event (name, date);
+CREATE INDEX hef_event_patient_name_date ON hef_event (patient_id, name, date);
 CREATE INDEX emr_labresult_native_code_and_result_float ON emr_labresult (native_code, result_float);
 CREATE INDEX emr_labresult_native_code_and_result_string ON emr_labresult (native_code, result_string);
 CREATE INDEX emr_labresult_code_high_float ON emr_labresult (native_code, ref_high_float, result_float);
