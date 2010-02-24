2e59970c7cc7823ba1160d57a3a0847a06f7bbaf
Jason McVetta <jason.mcvetta@gmail.com>
2010-02-24 21:02:51 +0000

    PostgreSQL-specific SQL to create multi-column indices on database creation.
    Former-commit-id: c56728da0bb7c61d4be4a6a1628436908e710ec7

---
+++ src/ESP/emr/sql/multicolumn-indices.postgresql.sql
@@ -0,0 +1,11 @@
+--------------------------------------------------------------------------------
+--
+--                           Multi-Column Indices
+--
+--
+-- These indices provide significant performance improvements for our queries.
+--------------------------------------------------------------------------------
+CREATE INDEX hef_event_name_date ON hef_event (name, date);
+CREATE INDEX emr_labresult_native_code_and_result_float ON emr_labresult (native_code, result_float);
+CREATE INDEX emr_labresult_native_code_and_result_string ON emr_labresult (native_code, result_string);
+CREATE INDEX emr_labresult_code_high_float ON emr_labresult (native_code, ref_high_float, result_float);
