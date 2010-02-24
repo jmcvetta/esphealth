237683409f50ec1ac9142df92c40041254df7e12
Raphael Lullis <raphael@lullis.net>
2010-02-24 15:31:21 +0000

    daily_batch is going to be replaced by a django command, but we can keep it here for now.
    Former-commit-id: 4f9f54bc80fdf63124a4a7d6dc5ecd2fd0631792

--- src/scripts/daily_batch.sh
+++ src/scripts/daily_batch.sh
@@ -20,18 +20,8 @@ else
   DATE=`date -d "-1 day" +%Y%m%d`
 fi
 
-
-#
-# Sync to PostgreSQL
-#
-# PostgreSQL JDBC driver issues necessitate that the sync be done in two
-# stages, using different driver versions.
-#
-/opt/pdi-3.1.0-pg7.4/kitchen.sh -rep pdi_3_1_0 -user esp -dir prod-sync -job prod-sync &&
-/opt/pdi-3.1.0-pg8.4/pan.sh -rep pdi_3_1_0 -user esp -dir prod-sync -trans "sync encounter icd9s" &&
-python $ESP_HOME/ss/main.py -f --begin=$DATE --end=$DATE &&
-source $BASEDIR/upload_ss_reports.sh &&
-python $ESP_HOME/vaers/main.py -c -r -a --begin=$DATE --end=$DATE &&
-source $BASEDIR/upload_vaers_reports.sh &&
-source $BASEDIR/upload_satscan_reports.sh 
+python $ESP_HOME/manage.py download_ftp --begin=$DATE &&
+python $ESP_HOME/manage.py load_epic &&
+python $ESP_HOME/ss/main.py -f --begin=$DATE --end=$DATE
+python $ESP_HOME/vaers/main.py -c -r -a --begin=$DATE --end=$DATE
 
