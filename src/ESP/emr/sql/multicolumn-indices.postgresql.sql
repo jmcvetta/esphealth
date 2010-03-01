e63f491776183ddad2c7d57fb8569b7b4d7d04e5
Jason McVetta <jason.mcvetta@gmail.com>
2010-03-01 18:50:07 +0000

    Add native_code, native_name mutli-column index on emr_labresult, to speed concordance generation.
    Former-commit-id: 9603a2664e1b42f91d4f43a147442ec232027b98

--- src/ESP/emr/sql/multicolumn-indices.postgresql.sql
+++ src/ESP/emr/sql/multicolumn-indices.postgresql.sql
@@ -10,3 +10,4 @@ CREATE INDEX hef_event_patient_name_date ON hef_event (patient_id, name, date);
 CREATE INDEX emr_labresult_native_code_and_result_float ON emr_labresult (native_code, result_float);
 CREATE INDEX emr_labresult_native_code_and_result_string ON emr_labresult (native_code, result_string);
 CREATE INDEX emr_labresult_code_high_float ON emr_labresult (native_code, ref_high_float, result_float);
+CREATE INDEX emr_labresult_native_code_native_name ON emr_labresult (native_code, native_name);
