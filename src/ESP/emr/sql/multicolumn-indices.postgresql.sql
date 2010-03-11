3c829b062be8ab6a3467eb4c355cc840f8be6fc3
Jason McVetta <jason.mcvetta@gmail.com>
2010-03-11 18:59:27 +0000

    More multi-column indices -- need to evaluate if these are actually used by HEF queries, to determine if they are worth the db load.
    Former-commit-id: f4c436dbd4e4d6f21711e3089d3904d70a235334

--- src/ESP/emr/sql/multicolumn-indices.postgresql.sql
+++ src/ESP/emr/sql/multicolumn-indices.postgresql.sql
@@ -10,4 +10,8 @@ CREATE INDEX hef_event_patient_name_date ON hef_event (patient_id, name, date);
 CREATE INDEX emr_labresult_native_code_and_result_float ON emr_labresult (native_code, result_float);
 CREATE INDEX emr_labresult_native_code_and_result_string ON emr_labresult (native_code, result_string);
 CREATE INDEX emr_labresult_code_high_float ON emr_labresult (native_code, ref_high_float, result_float);
+CREATE INDEX emr_labresult_ts_native_code_and_result_float ON emr_labresult (updated_timestamp, native_code, result_float);
+CREATE INDEX emr_labresult_ts_native_code_and_result_string ON emr_labresult (updated_timestamp, native_code, result_string);
+CREATE INDEX emr_labresult_ts_code_high_float ON emr_labresult (updated_timestamp, native_code, ref_high_float, result_float);
 CREATE INDEX emr_labresult_native_code_native_name ON emr_labresult (native_code, native_name);
+CREATE INDEX emr_prescription_ts_name_like ON emr_prescription (updated_timestamp, name text_pattern_ops);
