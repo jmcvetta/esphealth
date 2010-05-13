0a0127fa58eb988615c32dad56531b9c537ffdfa
Jason McVetta <jason.mcvetta@gmail.com>
2010-05-13 20:16:39 +0000

    Add new 'install' command -- hacked directly into manage.py, since Django command framework cannot be used while settings are not configured.
    Former-commit-id: 3f252a44967910b183687544715426a785757f90

--- src/ESP/manage.py
+++ src/ESP/manage.py
@@ -22,6 +22,59 @@ if not os.environ.has_key('DJANGO_SETTINGS_MODULE'):
     logging.log(logging.DEBUG, 'DJANGO_SETTINGS_MODULE:')
     logging.log(logging.DEBUG, '\t%s' % os.environ['DJANGO_SETTINGS_MODULE'])
 
+#
+# Intercept the special argument 'install' -- if someone is running this
+# command, presumably 'import settings' will fail, because settings files do
+# not yet exists.
+#
+if sys.argv[1] == 'install':
+    _folders = [
+        os.path.join('/', 'srv', 'esp-data'),
+        os.path.join('/', 'srv', 'esp-data', 'case_reports'),
+        os.path.join('/', 'srv', 'esp-data', 'epic'),
+        os.path.join('/', 'srv', 'esp-data', 'epic', 'incoming'),
+        os.path.join('/', 'srv', 'esp-data', 'epic', 'archive'),
+        os.path.join('/', 'srv', 'esp-data', 'epic', 'error'),
+        os.path.join('/', 'srv', 'esp-data', 'hl7'),
+        os.path.join('/', 'srv', 'esp-data', 'hl7', 'incoming'),
+        os.path.join('/', 'srv', 'esp-data', 'hl7', 'archive'),
+        os.path.join('/', 'srv', 'esp-data', 'hl7', 'error'),
+        ]
+    print 'Checking .ini files...'
+    #
+    # The following code is copied verbatim from settings.py -- cannot just
+    # import settings, because exit with errors when no .ini files exist.
+    #
+    from configobj import ConfigObj
+    from validate import Validator
+    TOPDIR = os.path.dirname(__file__)
+    PACKAGE_ROOT = os.path.normpath(os.path.join(TOPDIR, '..', '..'))
+    CONFIG_FOLDER = os.path.join(PACKAGE_ROOT, 'etc')
+    validator = Validator()
+    esp_ini = os.path.join(CONFIG_FOLDER, 'esp.ini')
+    esp_spec = ConfigObj(os.path.join(TOPDIR, 'esp.spec.ini'), interpolation=False, list_values=False)
+    secrets_ini = os.path.join(CONFIG_FOLDER, 'secrets.ini')
+    secrets_spec = os.path.join(TOPDIR, 'secrets.spec.ini')
+    ConfigObj(esp_ini, configspec=esp_spec, interpolation=False).validate(validator, copy=True)
+    print '    esp.ini'
+    ConfigObj(secrets_ini, configspec=secrets_spec).validate(validator, copy=True)
+    print '    secrets.ini'
+    #
+    #
+    print 'Checking data folders...'
+    for folder in _folders:
+        if not os.path.isdir(folder):
+            try:
+                os.mkdir(folder)
+            except OSError:
+                print
+                print 'You do not have permission to create folder: %s' % folder
+                print
+                print 'Please fix this, and try running install command again.'
+                print
+                sys.exit()
+            print 'Added new folder:  %s' % folder
+    sys.exit()
 
 from django.core.management import execute_manager
 
