20903a96de71158ea8850674423219b44b829b36
Raphael Lullis <raphael@lullis.net>
2010-05-27 19:05:02 +0000

    emr faker not really being used right now, but it might be handy to create fake (test) data and save to the database.
    Former-commit-id: 6881b0bc760bd5034360f3234ffb3acd5790a5ea

--- src/ESP/emr/fake.py
+++ src/ESP/emr/fake.py
@@ -1,27 +1,29 @@
+import datetime
+
 from ESP.emr.models import Provider, Patient
+from ESP.conf.common import EPOCH
+
+TODAY = datetime.date.today()
 
 PROVIDERS = [
-    Provider(first_name='Michael',
-             last_name='Klompas'),
-    Provider(first_name='Michael',
-             last_name='Lee'),
-    Provider(first_name='Raphael',
-             last_name='Lullis'),
-    Provider(first_name='Ross',
-             last_name='Lazarus'),
-    Provider(first_name='Jason',
-             last_name='McVetta')             
+    Provider(first_name='Michael', last_name='Klompas'),
+    Provider(first_name='Michael', last_name='Lee'),
+    Provider(first_name='Raphael', last_name='Lullis'),
+    Provider(first_name='Ross',    last_name='Lazarus'),
+    Provider(first_name='Jason',   last_name='McVetta')             
 ]
 
-def fiat_mondo(population_size=200):
-    for klass in [Provider, Patient]:
-        klass.delete_fakes()
+def fiat_mondo(population_size=200, start_date=EPOCH, end_date=TODAY,
+               save_to_epic=False, save_on_db=False):
+    Provider.make_fakes(save_on_db=save_on_db, save_to_epic=save_to_epic)
+    Patient.make_fakes(population_size, 
+                       save_on_db=save_on_db, save_to_epic=save_to_epic,
+                       make_lab_results=True, make_encounters=True,
+                       make_immunizations=True)
     
-    Provider.make_fakes()
-    Patient.make_fakes(population_size)
         
 if __name__ == '__main__':
-    fiat_mondo()
+    fiat_mondo(population_size=10**7, save_on_db=True)
   
 
 
