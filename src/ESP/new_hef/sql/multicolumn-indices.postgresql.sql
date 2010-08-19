6fe79fd7d03d4b25f25a9c4661bd15049a52d1ed
Jason McVetta <jason.mcvetta@gmail.com>
2010-08-19 03:16:43 +0000

    Remove defunct new_hef folder
    Former-commit-id: d8744593c96414898c518bbe32c560130c64e70b

--- src/ESP/new_hef/sql/multicolumn-indices.postgresql.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------------------------------
---
---                           Multi-Column Indices
---
---
--- These indices provide significant performance improvements for our queries.
---------------------------------------------------------------------------------
-CREATE INDEX new_hef_event_heuristic_content_type ON new_hef_event (heuristic_id, content_type_id);
