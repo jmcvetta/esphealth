ae7d9db5e3a55b809ad7df72b06aaccc46638446
Jason McVetta <jason.mcvetta@gmail.com>
2010-08-25 17:14:00 +0000

    Update multi-column indices for new schema
    Former-commit-id: c2ed378acde9c0912b643e007692a75590661f10

--- src/ESP/hef/sql/multicolumn-indices.postgresql.sql
+++ src/ESP/hef/sql/multicolumn-indices.postgresql.sql
@@ -5,7 +5,7 @@
 --
 -- These indices provide significant performance improvements for our queries.
 --------------------------------------------------------------------------------
-CREATE INDEX hef_event_name_date ON hef_event (name, date);
-CREATE INDEX hef_event_patient_name_date ON hef_event (patient_id, name, date);
-CREATE INDEX hef_timespan_patient_name ON hef_timespan (patient_id, name);
-CREATE INDEX hef_timespan_patient_name_dates ON hef_timespan (patient_id, name, start_date, end_date);
+CREATE INDEX hef_event_event_type_date ON hef_event (event_type_id, date);
+CREATE INDEX hef_event_patient_event_type_date ON hef_event (patient_id, event_type_id, date);
+-- CREATE INDEX hef_timespan_patient_event_type ON hef_timespan (patient_id, event_type_id);
+-- CREATE INDEX hef_timespan_patient_event_type_dates ON hef_timespan (patient_id, event_type_id, start_date, end_date);
