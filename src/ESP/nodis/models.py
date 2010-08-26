ec47573286184939640eb7ed560504592f305055
Jason McVetta <jason.mcvetta@gmail.com>
2010-08-26 18:45:53 +0000

    A Case's events before/after/ever should be nullable.

--- src/ESP/nodis/models.py
+++ src/ESP/nodis/models.py
@@ -1623,11 +1623,16 @@ class Case(models.Model):
     #
     # Events that define this case
     #
-    events = models.ManyToManyField(Event, blank=False) # The events that caused this case to be generated
-    # TODO: rename to events_before
-    events_before = models.ManyToManyField(Event, blank=False, related_name='case_before') # The events that caused this case to be generated, but occured before the event window
-    events_after = models.ManyToManyField(Event, blank=False, related_name='case_after') # The events that caused this case to be generated, but occurred after the event window
-    events_ever = models.ManyToManyField(Event, blank=False, related_name='case_ever') # The events that caused this case to be generated, but occurred after the event window
+    #
+    # The events that caused this case to be generated
+    events = models.ManyToManyField(Event, blank=False)
+    # The events that caused this case to be generated, but occured before the event window
+    events_before = models.ManyToManyField(Event, blank=True, null=True, related_name='case_before')
+    # The events that caused this case to be generated, but occurred after the event window
+    events_after = models.ManyToManyField(Event, blank=True, null=True, related_name='case_after')
+    # The events that caused this case to be generated, but occurred after the event window
+    events_ever = models.ManyToManyField(Event, blank=True, null=True, related_name='case_ever')
+    # The events that caused this case to be generated, but occurred after the event window
     
     def __get_condition_config(self):
         '''
