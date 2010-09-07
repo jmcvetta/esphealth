67d524cfdf7aea806b4c9986bffc050cd9c85522
Jason McVetta <jason.mcvetta@gmail.com>
2010-09-07 18:03:01 +0000

    Code to (re)generate the practice patient count table
    Former-commit-id: 0b3e414b95272e907d26c8a4179210d92bae57a9

--- src/ESP/phit/models.py
+++ src/ESP/phit/models.py
@@ -11,6 +11,9 @@ Public Health Intervention Tracker
 
 
 from django.db import models
+from django.db.models import Count
+from django.db.transaction import commit_on_success
+from dateutil.relativedelta import relativedelta
 
 from ESP.emr.models import Encounter
 from ESP.utils import log
@@ -27,7 +30,21 @@ class PracticePatients(models.Model):
     patient_count = models.IntegerField(blank=False)
     
     @classmethod
+    @commit_on_success
     def regenerate(cls):
-        month_list = Encounter.objects.dates('date', 'month')
-        log_query('month_list', month_list)
-        print month_list
+        PracticePatients.objects.all().delete() # Truncate table before repopulating
+        end_date = None
+        month_start_date = None
+        for month_end_date in Encounter.objects.dates('date', 'month'):
+            if not month_start_date:
+                month_start_date = month_end_date                
+                continue
+            end_date = month_end_date
+            begin_date = month_start_date - relativedelta(years=3)
+            encs = Encounter.objects.filter(date__gte=begin_date, date__lt=end_date)
+            prac_pts = encs.values('patient').annotate(enc_count=Count('pk')).filter(enc_count__gte=2)
+            #log_query('practice patients for %s' % begin_date, prac_pts)
+            prac_pt_count = prac_pts.count()
+            PracticePatients(month=month_start_date, patient_count=prac_pt_count).save()
+            log.debug('Practice patients for %s: %s' % (month_start_date, prac_pt_count))
+            month_start_date = month_end_date
