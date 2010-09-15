9ace4ecaccc708e984ec82a1761a71f948602e1d
Jason McVetta <jason.mcvetta@gmail.com>
2010-09-15 10:25:37 +0000

    Add linelist support to phit command
    Former-commit-id: 279fa0042bfc6859aad7e49ee564c9fdbc80c643

--- src/ESP/phit/management/commands/phit.py
+++ src/ESP/phit/management/commands/phit.py
@@ -20,6 +20,7 @@ from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand
 from django.db.models import Q, F
 
+from ESP.emr.models import Patient
 from ESP.phit.models import MonthlyStatistics
 from ESP.hef.models import Event
 from ESP.utils import log
@@ -37,6 +38,15 @@ class Command(BaseCommand):
     
     help = 'Generate counts of practice patients per month'
     
+    option_list = BaseCommand.option_list + (
+        make_option('--no-regenerate-statistics', action='store_false', dest='regenerate', 
+            help='Do not regenerate montly statistics before producing report', default=True),
+        make_option('--summary', action='store_true', dest='summary', default=False,
+            help='Produce monthly summary report'),
+        make_option('--linelist', action='store_true', dest='linelist', default=False,
+            help='Produce patient line list report'),
+        )
+    
     QUERIES = {
         'ogtt': Q(event_type__name__startswith='ogtt', event_type__name__endswith='--any_result'),
         'a1c': Q(event_type__name='a1c--any_result'),
@@ -44,8 +54,22 @@ class Command(BaseCommand):
         }
     
     def handle(self, *args, **options):
-        log.info('Regenerating monthly statistics')
-        MonthlyStatistics.regenerate()
+        if options['regenerate']:
+            log.info('Regenerating monthly statistics')
+            MonthlyStatistics.regenerate()
+        if options['linelist']:
+            self.linelist()
+        elif options['summary']:
+            self.summary()
+        else:
+            sys.stderr.write('You must choose either --summary or --linelist\n')
+            sys.exit(-1)
+    
+    
+    def summary(self):
+        '''
+        Produces monthly summary report
+        '''
         field_names = ['month', 'practice_patients', 'total_encounters', 'patients_with_encounter', 'any_test']
         field_names += self.QUERIES.keys()
         writer = csv.DictWriter(sys.stdout, field_names)
@@ -66,3 +90,30 @@ class Command(BaseCommand):
                 values[name] = events.filter(self.QUERIES[name]).values('patient').distinct().count()
             log.debug(values)
             writer.writerow(values)
+    
+    def linelist(self):
+        field_names = ['month', 'practice_patients', 'total_encounters', 'patients_with_encounter', 'patient_last', 'patient_first', 'patient_mrn', 'patient_age']
+        field_names += self.QUERIES.keys()
+        writer = csv.DictWriter(sys.stdout, field_names)
+        for ms in MonthlyStatistics.objects.all():
+            next_month = ms.month + relativedelta(months=1)
+            events = Event.objects.filter(date__gte=ms.month, date__lte=next_month)
+            any_q = Q(pk__isnull=True) # Null Q object
+            for name in self.QUERIES:
+                any_q |= self.QUERIES[name]
+            for pat_id in events.filter(any_q).values_list('patient', flat=True).distinct():
+                patient = Patient.objects.get(pk=pat_id)
+                values = {
+                    'month': ms.month,
+                    'practice_patients': ms.practice_patients,
+                    'total_encounters': ms.total_encounters,
+                    'patients_with_encounter': ms.patients_with_encounter,
+                    'patient_last': patient.last_name,
+                    'patient_first': patient.first_name,
+                    'patient_mrn': patient.mrn,
+                    'patient_age': relativedelta(ms.month, patient.date_of_birth).years,
+                    }
+                for name in self.QUERIES:
+                    values[name] = bool( events.filter(self.QUERIES[name]).filter(patient=patient) )
+                log.debug(values)
+                writer.writerow(values)
