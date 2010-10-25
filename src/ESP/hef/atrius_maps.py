ebf687b5ae9eebcec5fc308b865084661fe0e829
Jason McVetta <jason.mcvetta@gmail.com>
2010-10-25 09:34:57 +0000

    Auto antibody tests
    Former-commit-id: 225896118875c5db2ef67dfc98da4c5e3aef59d8

--- src/ESP/hef/atrius_maps.py
+++ src/ESP/hef/atrius_maps.py
@@ -5,6 +5,9 @@ from ESP.hef.models import LabTestMap
 a1c = AbstractLabTest.objects.get(name='a1c')
 ogtt75_series = AbstractLabTest.objects.get(name = 'ogtt75_series')
 glucose_fasting = AbstractLabTest.objects.get(name='glucose_fasting')
+gad65 = AbstractLabTest.objects.get(name='gad65')
+ica512 = AbstractLabTest.objects.get(name='ica512')
+islet_cell_antibody = AbstractLabTest.objects.get(name='islet_cell_antibody')
 
 for c in [
     '80061--258',
@@ -77,3 +80,39 @@ for c in [
         print 'Already existed: %s' % obj
 
 
+#-------------------------------------------------------------------------------
+#
+# Diabetes Auto-Antibody Tests
+#
+#-------------------------------------------------------------------------------
+
+LabTestMap.objects.get_or_create(
+    test = gad65,
+    native_code = '83519--3571',
+    code_match_type = 'exact',
+    record_type = 'both',
+    threshold = 1.0,
+    )
+
+LabTestMap.objects.get_or_create(
+    test = ica512,
+    native_code = '86341--7163',
+    code_match_type = 'exact',
+    record_type = 'both',
+    threshold = 0.8,
+    )
+
+LabTestMap.objects.get_or_create(
+    test = islet_cell_antibody,
+    native_code = '86341--3421',
+    code_match_type = 'exact',
+    record_type = 'both',
+    )
+
+LabTestMap.objects.get_or_create(
+    test = islet_cell_antibody,
+    native_code = '86341--3422',
+    code_match_type = 'exact',
+    record_type = 'both',
+    threshold = 1.25,
+    )
