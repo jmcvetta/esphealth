953c5668488347e83200d37aac44b2274ce037d0
Jason McVetta <jason.mcvetta@gmail.com>
2011-01-27 20:09:30 +0000

    Experimental multi-column indices for GDM report, per ticket #181.
    Former-commit-id: 2a6c96a07902c0f90d0858f0968d5528c2c0f837

--- src/ESP/emr/sql/multicolumn-indices.postgresql.sql
+++ src/ESP/emr/sql/multicolumn-indices.postgresql.sql
@@ -13,7 +13,8 @@ CREATE INDEX emr_labresult_ts_native_code_and_result_string ON emr_labresult (up
 CREATE INDEX emr_labresult_ts_code_high_float ON emr_labresult (updated_timestamp, native_code, ref_high_float, result_float);
 CREATE INDEX emr_labresult_native_code_native_name ON emr_labresult (native_code, native_name);
 -- Optimization for reporting
-CREATE INDEX emr_labresult_native_code_date_patient ON emr_labresult (native_code, date, patient_id);
+CREATE INDEX emr_labresult_patient_date_code ON emr_labresult (patient_id, date, native_code);
+CREATE INDEX emr_labresult_patient_date_code_result ON emr_labresult (patient_id, date, native_code, result_float);
 CREATE INDEX emr_encounter_patient_date ON emr_encounter (patient_id, date);
 CREATE INDEX emr_encounter_patient_date_bmi ON emr_encounter (patient_id, date, bmi);
 CREATE INDEX emr_encounter_patient_date_height ON emr_encounter (patient_id, date, height);
