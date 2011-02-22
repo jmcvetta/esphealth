f61b343d32ac9f815644eaf3082dcda03ff79971
Jason McVetta <jason.mcvetta@gmail.com>
2011-02-22 21:54:35 +0000

    Add header & documentation to install.sh
    Former-commit-id: 5f4200fb715f39ead5decb8f7ec9bae9d2e5d86c

--- esphealth/install.sh
+++ esphealth/install.sh
@@ -1,3 +1,18 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                 ESP Health
+#                               Install Script
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# This script must be run by a user with write permission on the 'esphealth'
+# folder.  On Ubuntu we recommend NOT installing the pip system package via
+# apt-get, but instead installing pip with easy_install like this:
+# 
+#     $ sudo easy_install pip
+#
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set -x
 virtualenv .
 . bin/activate
