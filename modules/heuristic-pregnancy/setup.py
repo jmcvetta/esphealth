d2eb045d509653cc5e77648e7e20e582c72ab209
Jason McVetta <jason.mcvetta@gmail.com>
2011-03-03 01:21:43 +0000

    Pregnancy heuristic model, initial work
    Former-commit-id: ef7410bb19ba684e6a1978ddda094a8be73f1f69

---
+++ modules/heuristic-pregnancy/setup.py
@@ -0,0 +1,34 @@
+'''
+                                  ESP Health
+                             Packaging Information
+                                  
+@author: Jason McVetta <jason.mcvetta@heliotropi.cc>
+@organization: Channing Laboratory http://www.channing.harvard.edu
+@contact: http://esphealth.org
+@copyright: (c) 2011 Channing Laboratory
+@license: LGPL 3.0 - http://www.gnu.org/licenses/lgpl-3.0.txt
+'''
+
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'esphealth-heuristic-pregnancy',
+    version = '1.0',
+    author = 'Jason McVetta',
+    author_email = 'jason.mcvetta@heliotropi.cc',
+    description = 'Pregnancy heuristic module for ESP Health application',
+    license = 'LGPLv3',
+    keywords = 'pregnancy algorithm disease surveillance public health epidemiology',
+    url = 'http://esphealth.org',
+    packages = find_packages(exclude=['ez_setup']),
+    install_requires = [
+        'esphealth >= 3',
+        ],
+    entry_points = '''
+        [esphealth]
+        timespanheuristic = pregnancy.pregnancy
+    '''
+    )
