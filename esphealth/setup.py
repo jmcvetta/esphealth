4a99658387ce106b0814e1b0c2fa563abf0cebbb
Jason McVetta <jason.mcvetta@gmail.com>
2011-04-11 06:51:08 +0000

    Fixme note in setup.py
    Former-commit-id: 7cd2741d7fff858f0af2820426e3863e221f57fa

--- esphealth/setup.py
+++ esphealth/setup.py
@@ -12,6 +12,10 @@
 from setuptools import setup
 from setuptools import find_packages
 
+#
+# FIXME: Update this file to reflect current ESP setup
+#
+'''
 setup(
     name = 'esphealth-core',
     version = '3.0',
@@ -37,3 +41,4 @@ setup(
         'ui': ['*.html', '*.txt',], 
         },
     )
+'''
