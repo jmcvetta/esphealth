4d2925de4d60f4a61cabd3b90d5095ebf2c40142
Jason McVetta <jason.mcvetta@gmail.com>
2011-05-03 20:51:54 +0000

    Packaging improvements in preparation for candidate release

--- esphealth/install.sh
+++ esphealth/install.sh
@@ -13,9 +13,16 @@
 #
 #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# TODO: Check OS version too
+if [ `lsb_release -is` = "Ubuntu" ]; then
+    sudo apt-get install python-setuptools python-dev libpq-dev
+    sudo easy_install -U pip virtualenv
+fi
+
 set -x
-virtualenv --no-site-packages .
+virtualenv --no-site-packages . 
 . bin/activate
 export PIP_RESPECT_VIRTUALENV=true
 export PIP_REQUIRE_VIRTUALENV=true
-pip install -v -r requirements.txt
+pip install -v -r requirements.frozen.txt
