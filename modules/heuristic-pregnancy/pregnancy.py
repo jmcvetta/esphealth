3135b7a930a8cc012a6fea587d398915acb7c734
Jason McVetta <jason.mcvetta@gmail.com>
2011-06-07 22:20:47 +0000

    Major rewrite of pregnancy inference algorithm.  This version is still generating a few overlaps, so is not yet ready for production.
    Former-commit-id: 27228190f03d92faf4b814d624a61b9d7cc48985

--- modules/heuristic-pregnancy/pregnancy.py
+++ modules/heuristic-pregnancy/pregnancy.py
@@ -64,18 +64,16 @@ class PregnancyHeuristic(BaseTimespanHeuristic):
         # EDD Encounters
         #
         #-------------------------------------------------------------------------------
-        edd_enc_qs = Encounter.objects.all()
-        edd_enc_qs = edd_enc_qs.filter(edd__isnull=False)
-        self.edd_enc_qs = edd_enc_qs.exclude(timespan__name__in=self.timespan_names)
+        self.edd_enc_qs = Encounter.objects.filter(edd__isnull=False)
         #-------------------------------------------------------------------------------
         #
         # ICD9 Encounters
         #
         #-------------------------------------------------------------------------------
-        preg_icd9_q = Q(icd9_codes__code__startswith='V22.') | Q(icd9_codes__code__startswith='V23.')
-        icd9_enc_qs = Encounter.objects.all()
-        icd9_enc_qs = icd9_enc_qs.filter(preg_icd9_q)
-        self.icd9_enc_qs = icd9_enc_qs.exclude(timespan__name__in=self.timespan_names)
+        self.icd9_enc_qs = Encounter.objects.filter(
+            Q(icd9_codes__code__startswith='V22.') | 
+            Q(icd9_codes__code__startswith='V23.')
+            )
         #-------------------------------------------------------------------------------
         #
         # End of Pregnancy
@@ -152,24 +150,22 @@ class PregnancyHeuristic(BaseTimespanHeuristic):
         preg_end_q |= Q(icd9_codes__code__startswith='668.') 
         preg_end_q |= Q(icd9_codes__code__startswith='669.') 
         #
-        # Encounter QS
+        # End of Pregnancy (EoP)
         #
-        eop_qs = Encounter.objects.all() # EoP = End of Pregnancy
-        eop_qs = eop_qs.filter(preg_end_q)
-        self.eop_qs = eop_qs
+        self.eop_qs = Encounter.objects.filter(preg_end_q)
         #
         # Relevant encounters
         #
-        preg_enc_qs = edd_enc_qs | icd9_enc_qs
-        self.preg_enc_qs = preg_enc_qs.exclude(timespan__name__in=self.timespan_names)
-        self.relevant_enc_qs = edd_enc_qs | icd9_enc_qs | eop_qs # Does NOT exclude bound events!!
+        self.onset_qs = self.edd_enc_qs | self.icd9_enc_qs
+        self.relevant_enc_qs = self.onset_qs | self.eop_qs
         
     def generate(self):
         log.info('Generating pregnancy events')
         #
         # Get possible patients
         #
-        patient_qs = self.preg_enc_qs.values_list('patient', flat=True).order_by('patient').distinct()
+        unbound_onset_qs = self.onset_qs.exclude(timespan__name='pregnancy')
+        patient_qs = Patient.objects.filter(encounter__in=unbound_onset_qs).distinct()
         log_query('Pregnant patients', patient_qs)
         funcs = [partial(self.pregnancies_for_patient, patient) for patient in patient_qs]
         ts_count = wait_for_threads(funcs)
@@ -222,6 +218,127 @@ class PregnancyHeuristic(BaseTimespanHeuristic):
     
     @transaction.commit_on_success
     def pregnancies_for_patient(self, patient):
+        counter = 0
+        unbound_qs = self.onset_qs.filter(patient=patient)
+        unbound_qs = unbound_qs.exclude(timespan__name='pregnancy')
+        unbound_qs = unbound_qs.order_by('date')
+        for enc in unbound_qs:
+            # 
+            # Does this encounter belong to another pregnancy?
+            #
+            existing_preg_qs = Timespan.objects.filter(name='pregnancy')
+            existing_preg_qs = existing_preg_qs.filter(patient=patient)
+            existing_preg_qs = existing_preg_qs.filter(start_date__lte=enc.date)
+            existing_preg_qs = existing_preg_qs.filter(end_date__gte=enc.date)
+            existing_preg_qs = existing_preg_qs.order_by('start_date') # First existing preg
+            if existing_preg_qs:
+                preg_ts = existing_preg_qs[0]
+                preg_ts.encounters.add(enc)
+                log.debug('Added %s to existing pregnancy %s' % (enc, preg_ts))
+                continue
+            #
+            preg_start = None
+            preg_end   = None
+            pattern    = None
+            #
+            # Encounter date within the 280 days preceding EDD
+            #
+            edd_qs = self.edd_enc_qs.filter(patient=patient)
+            edd_qs = edd_qs.filter(date__gte=enc.date)
+            # Maybe this should be more than 280 days, in case of late pregnancy 
+            # where final (most accurate) EDD comes after first enc + 280?
+            edd_qs = edd_qs.filter( date__lte = enc.date + relativedelta(days=280) )
+            edd_qs = edd_qs.order_by('-date') # Most recent EDD is most valid (?)
+            if edd_qs:
+                #
+                # Period of pregnancy defined as onset at (EDD-280 days) and end at EDD
+                #
+                preg_start = edd_qs[0].edd
+                preg_end = preg_start - relativedelta(days=280)
+                pattern = 'edd'
+            else:
+                #
+                # If no EDD available then patient is pregnant if ICD9 = V22.x or V23.x.
+                #
+                # (Since we passed the EDD section, any enc making it this far is an ICD9.)
+                #
+                # Pregnancy onset:
+                #
+                #     Date of first ICD9 for pregnancy (V22.x or V23.x) minus 30 days
+                #
+                preg_start = enc.date - relativedelta(days=30)
+                #
+                # Pregnancy end: 
+                #
+                #     ICD9 for postpartum care:             V24.x
+                #     Ectopic & molar pregnancy, abortion:  630.x - 637.x and 639
+                #     Complications of preg with delivery:  640.xx-649.xx with 5th digit = 1, 2 or 4
+                #     Normal delivery                       650.xx-659.xx with 5th digit = 1, 2 or 4
+                #     Complications of labour:              660 - 669            
+                #     Date of last ICD9 for pregnancy (V22.x or V23.x) plus 14 days
+                q_obj = Q(patient=patient)
+                q_obj &= Q(date__gte=preg_start)
+                q_obj &= Q( date__lte = enc.date + relativedelta(days=280) )
+                eop_qs = self.eop_qs.filter(q_obj).order_by('date') # First EoP
+                icd9_qs = self.icd9_enc_qs.filter(q_obj).order_by('-date') # Last ICD9
+                if eop_qs:
+                    preg_end = eop_qs[0].date
+                    pattern = 'eop'
+                else:
+                    preg_end = icd9_qs[0].date
+                    pattern = 'icd9'
+            #-------------------------------------------------------------------------------
+            #
+            # New Pregnancy
+            #
+            #-------------------------------------------------------------------------------
+            # 
+            # If patient has multiple qualifying events for preg_start and preg_end that 
+            # generate overlapping periods of pregnancy, use the minimum for each of 
+            # preg_start and preg_end
+            #
+            new_preg = Timespan(
+                patient = patient,
+                name = 'pregnancy',
+                start_date = preg_start,
+                end_date = preg_end,
+                pattern = pattern,
+                source = self.uri,
+                )
+            new_preg.save() # Must save before populating M2M
+            relevant_encounters = self.relevant_enc_qs.filter(patient=patient)
+            relevant_encounters = relevant_encounters.filter(date__gte=preg_start)
+            relevant_encounters = relevant_encounters.filter(date__lte=preg_end)
+            new_preg.encounters = relevant_encounters
+            new_preg.save()
+            counter += 1
+            log.info('Created new pregnancy: %s (%s)' % (new_preg, pattern))
+            #
+            # Check overlap - currently this is set up to provide verbose 
+            # debugging info.  However it should be developed into a true
+            # overlap-prevention technique.
+            #
+            overlap_qs = Timespan.objects.filter(name='pregnancy')
+            overlap_qs = overlap_qs.filter(patient=patient)
+            overlap_qs = overlap_qs.filter( 
+                ( Q(start_date__lte=preg_start) & Q(end_date__gte=preg_start) ) 
+                |
+                ( Q(start_date__lte=preg_end) & Q(end_date__gte=preg_end) )
+                )
+            overlap_qs = overlap_qs.exclude(pk=new_preg.pk)
+            if overlap_qs:
+                msg = 'Overlapping pregnancies!\n'
+                msg += '    encounter: %s\n' % enc.verbose_str
+                msg += '    new:       %s\n' % new_preg
+                for ts in overlap_qs:
+                    msg += '    existing:  %s\n' % ts
+                    for e in ts.encounters.all().order_by('date'):
+                        msg += '        %s\n' % e.verbose_str
+                log.warning(msg)
+        return counter
+        
+    @transaction.commit_on_success
+    def craptastic_pregnancies_for_patient(self, patient):
         counter = 0
         last_preg = None
         min_date_next_preg = None
