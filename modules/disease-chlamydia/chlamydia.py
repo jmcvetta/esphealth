b6d693e8938ce06cea02759f45ed883a1b7a127e
Jason McVetta <jason.mcvetta@gmail.com>
2011-10-18 02:15:31 +0000

    First draft of ESPv3 Chlamydia definition
    Former-commit-id: c9f8d389b00ff95caed71a2663d00f541215f865

---
+++ modules/disease-chlamydia/chlamydia.py
@@ -0,0 +1,47 @@
+'''
+                                  ESP Health
+                         Notifiable Diseases Framework
+                           Chlamydia Case Generator
+
+
+@author: Jason McVetta <jason.mcvetta@gmail.com>
+@organization: Channing Laboratory http://www.channing.harvard.edu
+@contact: http://www.esphealth.org
+@copyright: (c) 2010 Channing Laboratory
+@license: LGPL
+'''
+
+from ESP.nodis.base import SinglePositiveTestDiseaseDefinition
+
+
+
+
+
+class Chlamydia(SinglePositiveTestDiseaseDefinition):
+    '''
+    '''
+    
+    condition = 'chlamydia'
+    
+    uri = 'urn:x-esphealth:disease:channing:chlamydia:v1'
+    
+    short_name = 'chlamydia'
+    
+    test_name_search_strings = ['chlam',]
+    
+    test_names = ['chlamydia']
+    
+    
+#-------------------------------------------------------------------------------
+#
+# Packaging
+#
+#-------------------------------------------------------------------------------
+
+chlamydia_definition = Chlamydia()
+
+def event_heuristics():
+    return chlamydia_definition.event_heuristics
+
+def disease_definitions():
+    return [chlamydia_definition]
