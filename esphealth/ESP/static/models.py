141453e204bd635dbe110920c8958d632f00a35c
cchacin <cchacin@localhost>
2011-11-04 19:05:09 +0000

    added definitions for make meds, fake icd9s, changed fake labs and icd9s increased name length

--- esphealth/ESP/static/models.py
+++ esphealth/ESP/static/models.py
@@ -21,6 +21,48 @@
 
 from django.db import models
 
+class FakeMeds (models.Model):
+    '''
+     this is for our new extra med table 
+    '''
+    def _get_name(self):
+    
+        #Returns long common name if available, falling back to short name.
+        
+        if  self.long_name:
+            return self.long_name
+        else:
+            return self.ndc_code
+        
+    name = property(_get_name)
+   
+    fakemeds_id = models.AutoField(primary_key=True)
+    long_name = models.TextField(blank=True, null=True)
+    ndc_code = models.TextField(blank=True, null=True)  
+    route = models.TextField(blank=True, null=True)
+    weight = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'FAKEMEDS'
+
+    def __str__(self):
+        return '%s -- %s' % (self.fakemeds_id, self.name)
+    
+    
+class FakeICD9s(models.Model):
+    fakeicd9_id = models.AutoField(primary_key=True)
+    icd9_codes = models.CharField('icd9_codes', max_length=3350)
+    group_name = models.CharField('group_name', max_length=100)
+    list_length = models.IntegerField(blank=True, null=True)
+    weight = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return '%s -- %s' % (self.fakeicd9_id, self.name)
+    
+    class Meta:
+        verbose_name = 'FAKEICD9'
+
+  
 class FakeLabs (models.Model):
     '''
      this is for our new extra lab table 
@@ -34,11 +76,12 @@ class FakeLabs (models.Model):
         elif self.short_name:
             return self.short_name
         else:
-            return self.cpt_code
+            return self.native_code
     name = property(_get_name)
    
     fakelabs_id = models.AutoField(primary_key=True)
     short_name = models.TextField(blank=True, null=True)
+    long_name = models.TextField(blank=True, null=True)
     data_type = models.TextField(blank=True, null=True)
     normal_low = models.FloatField(blank=True, null=True)
     normal_high = models.FloatField(blank=True, null=True)
@@ -46,6 +89,9 @@ class FakeLabs (models.Model):
     very_low = models.FloatField(blank=True, null=True)
     very_high = models.FloatField(blank=True, null=True)
     cpt_code = models.TextField(blank=True, null=True)  
+    qualitative_values = models.TextField(blank=True, null=True) 
+    weight = models.FloatField(blank=True, null=True)
+    native_code = models.TextField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'FAKELABS'
@@ -163,7 +209,7 @@ class Loinc(models.Model):
 
 class Icd9(models.Model):
     code = models.CharField('ICD9 Code', max_length=10, primary_key=True)
-    name = models.CharField('Name', max_length=100,)
+    name = models.CharField('Name', max_length=150,)
 
     def __unicode__(self):
         return u'%s %s' % (self.code, self.name)
