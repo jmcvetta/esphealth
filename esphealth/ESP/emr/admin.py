e7fefc1d030eca4593379e054cf87def29d9baa9
Jason McVetta <jason.mcvetta@gmail.com>
2011-11-07 21:39:06 +0000

    Update admin field names for 3.0
    Former-commit-id: 9c25ffaad26968354611afb0cbdc1e0b4cd91956

--- esphealth/ESP/emr/admin.py
+++ esphealth/ESP/emr/admin.py
@@ -22,35 +22,35 @@ from ESP.emr.models import Immunization
 
 
 class ProviderAdmin(admin.ModelAdmin):
-    list_display = ['provider_id_num', 'name', 'dept']
-    search_fields = ['provider_id_num','last_name']
+    list_display = ['natural_key', 'name', 'dept']
+    search_fields = ['natural_key','last_name']
 
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ['patient_id_num', 'name', 'pcp', 'zip']
-    search_fields = ['patient_id_num', 'last_name']
+    list_display = ['natural_key', 'name', 'pcp', 'zip']
+    search_fields = ['natural_key', 'last_name']
 
 class LabResultAdmin(admin.ModelAdmin):
-    list_display = ['order_id_num', 'native_name', 'patient', 'provider', 'result_string']
-    list_display_links = ['order_id_num']
+    list_display = ['natural_key', 'native_name', 'patient', 'provider', 'result_string']
+    list_display_links = ['natural_key']
     raw_id_fields = ['patient', 'provider']
     search_fields = ['order_id_num', 'native_code', 'native_name', 'patient__mrn']
     ordering = ['-date']
 
 class EncounterAdmin(admin.ModelAdmin):
-    list_display = ['native_encounter_num', 'patient', 'provider', 'date']
-    list_display_links = ['native_encounter_num']
+    list_display = ['natural_key', 'patient', 'provider', 'date']
+    list_display_links = ['natural_key']
     raw_id_fields = ['patient', 'provider']
     search_fields = ['pk', 'native_encounter_num', 'patient__last_name', 'provider__last_name']
     ordering = ['-date']
 
 class PrescriptionAdmin(admin.ModelAdmin):
-    list_display = ['order_id_num', 'date', 'patient', 'name', 'dose']
-    search_fields = ['order_id_num', 'name', 'patient__last_name']
+    list_display = ['natural_key', 'date', 'patient', 'name', 'dose']
+    search_fields = ['natural_key', 'name', 'patient__last_name']
     ordering = ['-date']
 
 class ImmunizationAdmin(admin.ModelAdmin):
-    list_display = ['imm_id_num', 'date', 'patient', 'name']
-    search_fields = ['imm_id_num', 'name', 'patient__last_name']
+    list_display = ['natural_key', 'date', 'patient', 'name']
+    search_fields = ['natural_key', 'name', 'patient__last_name']
     ordering = ['-date']
 
 class ProvenanceAdmin(admin.ModelAdmin):
