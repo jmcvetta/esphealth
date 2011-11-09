9b25856787a119f25210d6654c8c8b360b3f4bc0
Jason McVetta <jason.mcvetta@gmail.com>
2011-11-09 21:30:29 +0000

    Revert chlamydia def to remove PID definition.
    Former-commit-id: f4889849d4e60cb8e27b64e7dcb446533a07cadf

--- modules/disease-chlamydia/setup.py
+++ modules/disease-chlamydia/setup.py
@@ -1,10 +1,10 @@
 '''
                                   ESP Health
-                       Pelvic Inflamatory Disease (PID) Definition
+                          Diabetes Disease Definition
                              Packaging Information
                                   
-@author: Carolina Chacin <cchacin@commoninf.com>
-@organization: commonwealth informatics http://www.commoninf.com
+@author: Jason McVetta <jason.mcvetta@heliotropi.cc>
+@organization: Channing Laboratory http://www.channing.harvard.edu
 @contact: http://esphealth.org
 @copyright: (c) 2011 Channing Laboratory
 @license: LGPL 3.0 - http://www.gnu.org/licenses/lgpl-3.0.txt
@@ -14,20 +14,20 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name = 'esphealth-disease-pid',
+    name = 'esphealth-disease-syphilis',
     version = '1.0',
-    author = 'Carolina Chacin',
-    author_email = 'cchacin@commoninf.com',
-    description = 'Pelvic Inflamatory disease definition module for ESP Health application',
+    author = 'Jason McVetta',
+    author_email = 'jason.mcvetta@gmail.com',
+    description = 'Syphilis disease definition module for ESP Health application',
     license = 'LGPLv3',
-    keywords = 'Pelvic Inflamatory algorithm disease surveillance public health epidemiology',
+    keywords = 'syphilis algorithm disease surveillance public health epidemiology',
     url = 'http://esphealth.org',
     packages = find_packages(exclude=['ez_setup']),
     install_requires = [
         ],
     entry_points = '''
         [esphealth]
-        disease_definitions = pid:disease_definitions
-        event_heuristics = pid:event_heuristics
+        disease_definitions = syphilis:disease_definitions
+        event_heuristics = syphilis:event_heuristics
     '''
     )
