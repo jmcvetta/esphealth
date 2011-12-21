c0186f0de4de6edbd0c8822495a4255c327e9cf7
cchacin <cchacin@localhost>
2011-12-21 14:01:22 +0000

    adding instructions to shut down eclipse
    Former-commit-id: 0504006188c172687c16bc7e2a959cab106968a6

--- esphealth/update.sh
+++ esphealth/update.sh
@@ -15,7 +15,8 @@ virtualenv --no-site-packages .
 . bin/activate
 export PIP_RESPECT_VIRTUALENV=true
 export PIP_REQUIRE_VIRTUALENV=true
-read -p "Press any key to start svn update or Ctrl+C to abort..."
+
+read -p "Make sure eclipse is shutdown. Press any key to start svn update or Ctrl+C to abort..."
 
 svn up
 pip install -U -v -r requirements.txt
