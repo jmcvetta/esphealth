347ba0fdc7e08c4b662c3067e7381b18d027107c
cchacin <cchacin@localhost>
2012-01-13 23:42:33 +0000

    adding extra sqls to run after syncdb for multicolumn indices
    Former-commit-id: f4fb85c175e55f8ec81e411dde378152caa590fc

--- esphealth/ESP/emr/sql/labresult.postgresql_psycopg2.sql
+++ esphealth/ESP/emr/sql/labresult.postgresql_psycopg2.sql
@@ -19,8 +19,8 @@ CREATE INDEX emr_encounter_patient_date ON emr_encounter (patient_id, date);
 CREATE INDEX emr_encounter_patient_date_bmi ON emr_encounter (patient_id, date, bmi);
 CREATE INDEX emr_encounter_patient_date_height ON emr_encounter (patient_id, date, height);
 CREATE INDEX emr_encounter_patient_date_weight ON emr_encounter (patient_id, date, weight);
-CREATE INDEX emr_encounter_patient_edc ON emr_encounter (patient_id, edc);
-CREATE INDEX emr_encounter_patient_date_edc ON emr_encounter (patient_id, date, edc);
+CREATE INDEX emr_encounter_patient_edd ON emr_encounter (patient_id, edd);
+CREATE INDEX emr_encounter_patient_date_edd ON emr_encounter (patient_id, date, edd);
 CREATE INDEX emr_prescription_ts_name_like ON emr_prescription (updated_timestamp, name text_pattern_ops);
 -- Optimization for GDM report
-CREATE INDEX emr_labresult_native_code_patient_date_result_float ON emr_labresult (native_code, patient, date, result_float);
+CREATE INDEX emr_labresult_native_code_patient_date_result_float ON emr_labresult (native_code, patient_id, date, result_float);
