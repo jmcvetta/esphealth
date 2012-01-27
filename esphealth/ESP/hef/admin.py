9ee365ca139eaa2bdef78b1eeb3368ca92789494
cchacin <cchacin@localhost>
2012-01-27 22:36:15 +0000

    added more admin screens
    Former-commit-id: 13a1fa7e49c112244ceafd5bd5fa9109eb8a7680

--- esphealth/ESP/hef/admin.py
+++ esphealth/ESP/hef/admin.py
@@ -8,10 +8,16 @@ from django.contrib import admin
 
 
 from ESP.hef.models import Event
+from ESP.hef.models import Timespan
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['name', 'date', 'patient', 'content_object']
+    list_display = ['pk', 'name', 'patient', 'date', 'content_object']
 
+
+class TimespanAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'patient', 'start_date', 'end_date',]
+    raw_id_fields = ['events']
 
 admin.site.register(Event, EventAdmin)
+admin.site.register(Timespan, TimespanAdmin)
