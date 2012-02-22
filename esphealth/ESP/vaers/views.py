95e35874e28a5f018c2283ce37c9ae4798649b52
cchacin <cchacin@localhost>
2012-02-22 23:31:35 +0000

    fixing reports for vaers
    Former-commit-id: 3b4494428d66e6e12d0cd8308de381142cd8b7c9

--- esphealth/ESP/vaers/views.py
+++ esphealth/ESP/vaers/views.py
@@ -96,7 +96,7 @@ def verify(request, key):
                     'case':case })
 
 
-    else:
+    else: # request.method == 'POST
         confirmed_id = request.POST.get('provider_confirmation', 0) and provider.id
         response = HttpResponseRedirect(reverse('present_case', kwargs={'id':case.id}))
         response.set_cookie('confirmed_id', confirmed_id)
