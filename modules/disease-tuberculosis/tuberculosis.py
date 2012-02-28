9c863eaf5eb0f45d519829bf043ded2ee44b8ee3
cchacin <cchacin@localhost>
2012-02-28 18:09:09 +0000

    fixes to main prescription heuristic, tested multiple prescription cases. debugging dx and lab order issue
    Former-commit-id: e1ddd7951eb8462008dbf046a50d16e113082521

--- modules/disease-tuberculosis/tuberculosis.py
+++ modules/disease-tuberculosis/tuberculosis.py
@@ -20,16 +20,9 @@ from dateutil.relativedelta import relativedelta
 from django.db import transaction
 from django.db.models import Avg, Count, F, Max, Min, Q, Sum
 from ESP.utils import log
-from ESP.hef.base import Event
-from ESP.hef.base import BaseEventHeuristic
-from ESP.hef.base import PrescriptionHeuristic
-from ESP.hef.base import Dose
-from ESP.hef.base import LabResultPositiveHeuristic
-from ESP.hef.base import LabOrderHeuristic
-from ESP.hef.base import DiagnosisHeuristic
-from ESP.hef.base import Icd9Query
-from ESP.nodis.base import DiseaseDefinition
-from ESP.nodis.base import Case
+from ESP.hef.base import Event, BaseEventHeuristic,LabResultAnyHeuristic,  PrescriptionHeuristic
+from ESP.hef.base import Dose, LabResultPositiveHeuristic, LabOrderHeuristic, DiagnosisHeuristic, Icd9Query
+from ESP.nodis.base import DiseaseDefinition, Case
 
 
 class Tuberculosis(DiseaseDefinition):
@@ -67,92 +60,84 @@ class Tuberculosis(DiseaseDefinition):
                 Icd9Query(starts_with='018.'),
                 ]
             ))
+        
+        # defining any lab heuristic to be able to map them
+        for test_name in [
+            'tuberculosis_pcr',
+            'tuberculosis_afb',
+            'tuberculosis_mycob',
+            ]:
+            heuristic_list.append( LabResultAnyHeuristic(
+                test_name = test_name,
+                date_field = 'result',
+                ) )
         #
         # Lab Orders
         #
-        heuristic_list.append( LabOrderHeuristic(
-            test_name = 'tuberculosis_pcr',
-            ))
-        heuristic_list.append( LabOrderHeuristic(
-            test_name = 'tuberculosis_afb',
-            ))
-        heuristic_list.append( LabOrderHeuristic(
-            test_name = 'tuberculosis_mycob',
-            ))
+        for test_name in [
+            'tuberculosis_pcr',
+            'tuberculosis_afb',
+            'tuberculosis_mycob',
+            ]:
+            heuristic_list.append( LabOrderHeuristic(
+                test_name = test_name,
+                ))
         #
         # Prescriptions
         #
         heuristic_list.append( PrescriptionHeuristic(
             name = 'pyrazinamide',
             drugs = [ 'pyrazinamide', 'PZA',],
-            exclude = 'CAPZA', 
-            ))
-        
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'moxifloxacin',
-            drugs = ['moxifloxacin',],
-            ))
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'ethambutol',
-            drugs = ['ethambutol',],
-            ))
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'rifampin',
-            drugs = ['rifampin',],
-            ))
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'rifabutin',
-            drugs = ['rifabutin',],
-            ))
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'rifapentine',
-            drugs = ['rifapentine',],
-            ))
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'streptomycin',
-            drugs = ['streptomycin',],
-            ))
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'para_aminosalicyclic_acid',
-            drugs = ['para_aminosalicyclic_acid',],
-            ))
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'kanamycin',
-            drugs = ['kanamycin',],
-            ))
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'capreomycin',
-            drugs = ['capreomycin',],
-            ))
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'cycloserine',
-            drugs = ['cycloserine',],
-            ))
-        # TODO define each
-        heuristic_list.append( PrescriptionHeuristic(
-            name = 'ethionamide',
-            drugs = ['ethionamide',],
+            exclude = ['CAPZA',], 
             ))
         
         heuristic_list.append( PrescriptionHeuristic(
             name = 'isoniazid',
             drugs = [ 'Isoniazid','INH',],
-            exclude = 'INHAL, INHIB', 
+            exclude = ['INHAL', 'INHIB',], 
             ))
+        
+        for drug in [
+            'moxifloxacin',
+            'ethambutol',
+            'rifampin',
+            'rifabutin',
+            'rifapentine',
+            'streptomycin',
+            'para_aminosalicyclic_acid',
+            'kanamycin',
+            'capreomycin',
+            'cycloserine',
+            'ethionamide',
+            ]:
+         
+            heuristic_list.append( PrescriptionHeuristic(
+                name = drug,
+                drugs = [drug],
+                ))
+        
         
         return heuristic_list
     
     def generate_def_c(self):
+        #
+        # Criteria Set #3 dx and 2 distinct prescription orders
+        #  
+        
         log.info('Generating cases for Tuberculosis Definition (C)')
         counter = 0
-        rx_event_names =  ['rx:rifabutin','rx:capreomycin','rx:ethionamide','rx:isoniazid','rx:pyrazinamide']
+        allrx_event_names =  ['rx:isoniazid','rx:pyrazinamide','rx:moxifloxacin', 'rx:ethambutol',
+                            'rx:rifampin','rx:rifabutin','rx:rifapentine','rx:streptomycin',
+                            'rx:para_aminosalicyclic_acid','rx:kanamycin','rx:capreomycin',
+                            'rx:cycloserine','rx:ethionamide',]
+           
         dx_qs = BaseEventHeuristic.get_events_by_name('dx:tuberculosis')
         dx_qs = dx_qs.exclude(case__condition=self.conditions[0])
         dx_qs = dx_qs.order_by('patient', 'date') 
         for dx_event in dx_qs:
             relevancy_begin = dx_event.date - relativedelta(days=60)
             relevancy_end = dx_event.date + relativedelta(days=60)
-            rx_qs = BaseEventHeuristic.get_events_by_name(name=rx_event_names)
+            rx_qs = BaseEventHeuristic.get_events_by_name(name=allrx_event_names)
             rx_qs = rx_qs.filter(patient=dx_event.patient)
             rx_qs = rx_qs.filter(date__gte=relevancy_begin, date__lte=relevancy_end)
             if rx_qs.values('name').distinct().count() >= 2:
@@ -183,7 +168,8 @@ class Tuberculosis(DiseaseDefinition):
             name__in = rx_ev_names,
             )
         #
-        # Criteria Set #2 (dx or lab order  prior 14 days or 60 after icd9 code) 
+        # Criteria Set #2 
+        # dx in the 14 days prior to the lab order or in the 60 days following the test order 
         #
         dx_ev_names = ['dx:tuberculosis']
         lx_ev_names = ['lx:tuberculosis_prc:order','lx:tuberculosis_mycob:order','lx:tuberculosis_afb:order'] 
@@ -194,40 +180,14 @@ class Tuberculosis(DiseaseDefinition):
             patient__event__date__gte = (F('date') - 60 ),
             patient__event__date__lte = (F('date') + 14 ),
             )
-        #
-        # Criteria Set #3 dx and 2 distinct prescription orders
-        #  
-        rxall_ev_names =  ['rx:rifabutin','rx:capreomycin','rx:ethionamide','rx:isoniazid','rx:pyrazinamide' ]
-        #  count of 2 or more of each rx
-        #  get list of pt for each list of meds and 
-        # get the count of events for each distinct patients.
-        dx_qs = BaseEv
-        potential_patient_qs = Patient.objects.filter(event__name='dx:tuberculosis')
-        rx_event_qs = Event.objects.filter(name=rxall_ev_names)
-        rx_event_qs = rx_event_qs.filter(patient__in=potential_patient_qs)
-        rx_event_qs = rx_event_qs.exclude(case__condition='tuberculosis')
-        rx_event_qs = rx_event_qs.order_by('patient', 'date')
-        for rx_event in rx_event_qs:
-            pass
-        #twice_vqs = twice_qs.annotate(count=Count('patient'))
-        #twice_vqs = twice_vqs.filter(count__gte=2)
-        #twice_patients = twice_vqs.values_list('patient')
         
-        dxrx_event_qs = Event.objects.filter(
-            name__in = dx_ev_names,
-            patient__in=twice_patients,
-            patient__event__name__in =  rxall_ev_names,
-            patient__event__date__gte = (F('date') - 60 ),
-            patient__event__date__lte = (F('date') + 60 ),
-            )
-            
         #
         # Combined Criteria
         #
-        combined_criteria_qs = rx_event_qs | dxlx_event_qs | dxrx_event_qs  
+        combined_criteria_qs = rx_event_qs | dxlx_event_qs   
         combined_criteria_qs = combined_criteria_qs.exclude(case__condition='tuberculosis')
         combined_criteria_qs = combined_criteria_qs.order_by('date')
-        all_event_names = rx_ev_names + dx_ev_names + lx_ev_names + rxall_ev_names
+        all_event_names = rx_ev_names + dx_ev_names + lx_ev_names 
         counter = 0
         for this_event in combined_criteria_qs:
             existing_cases = Case.objects.filter(
@@ -247,7 +207,7 @@ class Tuberculosis(DiseaseDefinition):
                 patient = this_event.patient,
                 provider = this_event.provider,
                 date = this_event.date,
-                criteria = 'combined tuberculosis criteria',
+                criteria = 'combined tuberculosis criteria a and b',
                 source = self.uri,
                 )
             new_case.save()
@@ -258,6 +218,8 @@ class Tuberculosis(DiseaseDefinition):
             new_case.save()
             log.info('Created new tuberculosis case: %s' % new_case)
             counter += 1
+            counter += self.generate_def_c()
+        
         return counter # Count of new cases
             
 #-------------------------------------------------------------------------------
