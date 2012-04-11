6aac60f0402d6440a5f56d86cb45d681a1444458
cchacin <cchacin@localhost>
2012-04-11 21:06:53 +0000

    more text corrections on form
    Former-commit-id: 36a8ac3d0da0c4029194f8fd842d26697352b423

--- esphealth/ESP/vaers/forms.py
+++ esphealth/ESP/vaers/forms.py
@@ -31,14 +31,14 @@ class CaseConfirmFormold(forms.Form):
     
 class CaseConfirmForm(forms.Form):   
     
-    action = forms.ChoiceField(label = 'Probable Adverse Event?',choices=CASE_RESPONSE_CHOICES, 
+    action = forms.ChoiceField(label = 'Possible Adverse Event?',choices=CASE_RESPONSE_CHOICES, 
                                widget=forms.RadioSelect)
     comment = forms.CharField(label = 'Please provide details so that we can refine our adverse event detection algorithms (optional)',
                                 widget=forms.Textarea)
     ishelpful = forms.ChoiceField(label = 'Please help us assess this automated adverse event reporting facility. Was this message helpful?', choices=CASE_YESNO_CHOICES, 
                                widget=forms.RadioSelect)
-    interrupts = forms.ChoiceField(label = 'Did this message interrupt your work flow?', choices=CASE_YESNO_CHOICES, 
+    interrupts = forms.ChoiceField(label = 'Did it interrupt your work flow?', choices=CASE_YESNO_CHOICES, 
                                widget=forms.RadioSelect)
-    messagetype =  forms.ChoiceField(label = 'Over the past month, the messages have been', choices=CASE_TYPE_CHOICES, 
+    messagetype =  forms.ChoiceField(label = 'Has the number of messages recently been', choices=CASE_TYPE_CHOICES, 
                                widget=forms.RadioSelect)
      
