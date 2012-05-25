da63f9282cade00bc18d200847b537e8fa8b752e
cchacin <cchacin@localhost>
2012-05-25 18:24:25 +0000

    added code to toggle text area comments and size of text area change
    Former-commit-id: c5112e28fedc94ccfd438d4779d3c185aa43749e

--- esphealth/ESP/vaers/forms.py
+++ esphealth/ESP/vaers/forms.py
@@ -29,13 +29,9 @@ class CaseConfirmForm(forms.Form):
     state = forms.ChoiceField(label = 'Possible Adverse Event?',choices=CASE_RESPONSE_CHOICES, 
                                widget=forms.RadioSelect)
     
-    #for yes : Please comment on the likelihood and severity of this possible event:
-    #TODO For no: Please provide details so that we can refine our adverse event detection algorithms
-
-    comment = forms.CharField(label = 'Please comment on the likelihood and severity of this possible event:',
-                               widget=forms.Textarea(attrs={'cols': '40', 'rows': '5'}))
+    comment = forms.CharField(widget=forms.Textarea(attrs={'cols': '55', 'rows': '5'}))
     
-    message_ishelpful = forms.ChoiceField(help_text ='Please help us assess this automated adverse event reporting tool', label = 'Was this message helpful?', choices=CASE_YESNO_CHOICES, 
+    message_ishelpful = forms.ChoiceField( label = 'Was this message helpful?', choices=CASE_YESNO_CHOICES, 
                                widget=forms.RadioSelect)
     interrupts_work = forms.ChoiceField(label = 'Did it interrupt your work flow?', choices=CASE_YESNO_CHOICES, 
                                widget=forms.RadioSelect)
