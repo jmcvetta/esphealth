878bd6369a626e959201531f53befe2486152445
bzambarano <bzambarano@localhost>
2012-05-25 16:31:04 +0000

    new encounter type mapping
    Former-commit-id: 7bff35edf09dc77f0712796684632e42c387f38b

---
+++ esphealth/src/esphealth-encountertypemap-metrohealth/setup.py
@@ -0,0 +1,32 @@
+'''
+                                  ESP Health
+                     Encounter-Type Mapper for MetroHealth
+                                 Package Setup
+
+@author: Bob Zambarano <bzambarano@commoninf.com>
+@organization: Commonwealth Informatics Inc.
+@contact: http://www.esphealth.org
+@copyright: (c) 2012 Commonwealth Informatics Inc.
+@license: LGPL
+'''
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'esphealth-encountertypemap-metrohealth',
+    version = '1.0',
+    author = 'Bob Zambarano',
+    author_email = 'bzambarano@commoninf.com',
+    description = 'MetroHealth encounter-type mapper for ESP Health application',
+    license = 'LGPLv3',
+    keywords = 'disease surveillance public health epidemiology metrohealth',
+    url = 'http://esphealth.org',
+    packages = find_packages(exclude=['ez_setup']),
+    install_requires = [
+        ],
+    entry_points = '''
+        [esphealth]
+        encountertypemap = metrohealth:encountertypemap
+    '''
+    )
