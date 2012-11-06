30d20aaf323f8225c0148e85a610a539e7b56cc0
cchacin <cchacin@localhost>
2012-11-06 22:07:38 +0000

    cleaner code to check for unmapped loader fields and handle errors gracefully

--- esphealth/ESP/emr/management/commands/load_epic.py
+++ esphealth/ESP/emr/management/commands/load_epic.py
@@ -315,7 +315,12 @@ class BaseLoader(object):
             for key in row:
                 if row[key]:
                     try:
-                        row[key] = smart_unicode(row[key].strip())
+                        if not key:
+                            errors += 1
+                            log.error('There is a value that does not correspond to a any field in line: %s for value: %s' % (cur_row,row[key]))
+                        else:
+                            row[key] = smart_unicode(row[key].strip())
+                    
                     except DjangoUnicodeDecodeError, e:
                         #
                         # Log character set errors to db
