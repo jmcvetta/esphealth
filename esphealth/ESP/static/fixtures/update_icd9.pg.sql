11ffc79c9bc059078aa7ab4fc58c91b07cc63fb4
bzambarano <bzambarano@localhost>
2013-09-24 03:16:34 +0000

    modifications made to update_icd9.pg.sql from testing
    Former-commit-id: 8dd20652f6c10e76922213388a7a2cea6b318736

--- esphealth/ESP/static/fixtures/update_icd9.pg.sql
+++ esphealth/ESP/static/fixtures/update_icd9.pg.sql
@@ -1,4 +1,14 @@
-﻿/*Use this query to confirm the names of the foreign key constraints
+﻿-- The following delete query cleans up static_icd9 of any unreferenced codes
+delete from static_icd9 t0
+where not exists (select null from emr_problem t1 where t1.icd9_id=t0.code)
+    and not exists (select null from emr_encounter_icd9_codes t1 where t1.icd9_id=t0.code)
+    and not exists (select null from conf_reportableicd9 t1 where t1.icd9_id=t0.code)
+    and not exists (select null from vaers_diagnosticseventrule_heuristic_defining_codes t1 where t1.icd9_id=t0.code)
+    and not exists (select null from vaers_diagnosticseventrule_heuristic_discarding_codes t1 where t1.icd9_id=t0.code)
+    and not exists (select null from emr_hospital_problem t1 where t1.icd9_id=t0.code);
+
+
+/*Use this query to confirm the names of the foreign key constraints
   that refer to static_icd9 in the alter table statements below that drop
   the constraints.
 SELECT tc.constraint_name, tc.table_name, ccu.table_name as foreign_table
@@ -27,21 +37,9 @@ ALTER TABLE static_icd9 rename to old_icd9;
 -- now run esp syncdb;
 -- now run esp loaddata using the new json file
 
--- The following delete query is for old IDC tables with 2-digit icd9 procedure codes
-/*
-delete from old_icd9 
-where name not like '%load_epic$'
-  and ((code ~ '^\d' and (position('.' in code)=3 or length(code)=2)) 
-    or (code ~ '^\D' and (position('.' in code)=4 or length(code)=3)));
-
-*/
-
 insert into static_icd9 (code, name, longname)
 select code, name, longname from old_icd9 t0
 where not exists (select null from static_icd9 t1 where t1.code=t0.code)
-
--- don't drop old_icd9 until you've checked the new table.
-drop table old_icd9;
 
 --now rebuild the foreign keys
 ALTER TABLE vaers_diagnosticseventrule_heuristic_discarding_codes
@@ -74,4 +72,5 @@ ALTER TABLE emr_encounter_icd9_codes
       REFERENCES static_icd9 (code) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE INITIALLY DEFERRED;
 
-
+-- don't drop old_icd9 until you've rebuilt the foreign keys.
+drop table old_icd9;
