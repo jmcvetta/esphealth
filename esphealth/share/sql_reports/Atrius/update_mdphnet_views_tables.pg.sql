32a59c99a33ddf1e236f40f051c40f45e5b86ac1
bzambarano <bzambarano@localhost>
2014-01-29 06:59:38 +0000

    Adding smoking to MDPHNET
    Former-commit-id: ceaf02715f9e89e085a2bf7704e5ca16e7d21582

--- esphealth/share/sql_reports/Atrius/update_mdphnet_views_tables.pg.sql
+++ esphealth/share/sql_reports/Atrius/update_mdphnet_views_tables.pg.sql
@@ -384,12 +384,12 @@ create table esp_temp_smoking as
                when t3.passive='Passive' then 'Passive'
                when t4.never='Never' then 'Never'
                else 'Not available' 
-           end as smoking::varchar(13), 
+           end as smoking, 
            t0.natural_key as patid
    from
      emr_patient t0
    left outer join 
-     (select t00.tobacco_use latest, t00.patient_id 
+     (select t00.tobacco_use ls atest, t00.patient_id 
       from emr_socialhistory t00
       inner join
       (select max(date) as maxdate, patient_id 
@@ -397,17 +397,17 @@ create table esp_temp_smoking as
        where tobacco_use is not null and tobacco_use<>''
        group by patient_id) t01 on t00.patient_id=t01.patient_id and t00.date=t01.maxdate) t1 on t0.id=t1.patient_id
    left outer join
-     (select max(val) yesOrQuit, patient_id
+     (select max(val) as yesOrQuit, patient_id
       from (select 'Quit'::text as val, patient_id
             from emr_socialhistory where tobacco_use in ('Yes','Quit')) t00
             group by patient_id) t2 on t0.id=t2.patient_id
    left outer join
-     (select max(val) passive, patient_id
+     (select max(val) as passive, patient_id
       from (select 'Passive'::text as val, patient_id
             from emr_socialhistory where tobacco_use ='Passive') t00
             group by patient_id) t3 on t0.id=t3.patient_id
    left outer join
-     (select max(val) never, patient_id
+     (select max(val) as never, patient_id
       from (select 'never'::text as val, patient_id
             from emr_socialhistory where tobacco_use ='Never') t00
             group by patient_id) t4 on t0.id=t4.patient_id;
