dc4c7e871741cf1026b1b8936054a4b894cc6f12
cchacin <cchacin@localhost>
2014-02-25 15:32:23 +0000

    separating code to relate only to the custom table specifically.
    Former-commit-id: 450776f586b8d6dba0a8791e815c2746c31ba3f8

--- esphealth/ESP/emr/sql/labresult.postgresql_psycopg2.sql
+++ esphealth/ESP/emr/sql/labresult.postgresql_psycopg2.sql
@@ -12,15 +12,8 @@ CREATE INDEX emr_labresult_ts_native_code_and_result_float ON emr_labresult (upd
 CREATE INDEX emr_labresult_ts_native_code_and_result_string ON emr_labresult (updated_timestamp, native_code, result_string);
 CREATE INDEX emr_labresult_ts_code_high_float ON emr_labresult (updated_timestamp, native_code, ref_high_float, result_float);
 CREATE INDEX emr_labresult_native_code_native_name ON emr_labresult (native_code, native_name);
+-- Optimization for GDM report
+CREATE INDEX emr_labresult_native_code_patient_date_result_float ON emr_labresult (native_code, patient_id, date, result_float);
 -- Optimization for reporting
 CREATE INDEX emr_labresult_patient_date_code ON emr_labresult (patient_id, date, native_code);
 CREATE INDEX emr_labresult_patient_date_code_result ON emr_labresult (patient_id, date, native_code, result_float);
-CREATE INDEX emr_encounter_patient_date ON emr_encounter (patient_id, date);
-CREATE INDEX emr_encounter_patient_date_bmi ON emr_encounter (patient_id, date, bmi);
-CREATE INDEX emr_encounter_patient_date_height ON emr_encounter (patient_id, date, height);
-CREATE INDEX emr_encounter_patient_date_weight ON emr_encounter (patient_id, date, weight);
-CREATE INDEX emr_encounter_patient_edd ON emr_encounter (patient_id, edd);
-CREATE INDEX emr_encounter_patient_date_edd ON emr_encounter (patient_id, date, edd);
-CREATE INDEX emr_prescription_ts_name_like ON emr_prescription (updated_timestamp, name text_pattern_ops);
--- Optimization for GDM report
-CREATE INDEX emr_labresult_native_code_patient_date_result_float ON emr_labresult (native_code, patient_id, date, result_float);
