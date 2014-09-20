fa7f4dfcde53e8e6cf9daa82b9c05251b497f185
bzambarano <bzambarano@localhost>
2014-09-20 20:54:39 +0000

    Modifications for Django 1.4 compatibility
    Former-commit-id: 1653f1b9f523bf7c47bd5ef4feab4b5eee1906e9

--- esphealth/ESP/settings.py
+++ esphealth/ESP/settings.py
@@ -168,6 +168,7 @@ ROOT_URLCONF = 'ESP.urls'
 LOGIN_URL = config['Web']['login_url']
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
