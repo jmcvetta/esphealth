ea942758e5daa42ffb203db64c4830aa921cfcf0
cchacin <cchacin@localhost>
2015-03-07 10:40:02 +0000

    updated asthma plugin using lists and doing less queries. also processing unbound events.
    Former-commit-id: ebeb7bad56c46d7bc70a5cb1f55a2a2b04eceab1

--- modules/disease-asthma/asthma.py
+++ modules/disease-asthma/asthma.py
@@ -1,7 +1,7 @@
 '''
                                   ESP Health
                          Notifiable Diseases Framework
-                           Asthma Case Generator
+                           Asthma  Case Generator
 
 
 @author: Carolina Chacin <cchacin@commoninf.com>
@@ -35,9 +35,9 @@ from ESP.static.models import DrugSynonym
 
 
 
-class Asthma(DiseaseDefinition):
+class AsthmaNew(DiseaseDefinition):
     '''
-    Asthma
+    Asthma new
     '''
     
     conditions = ['asthma']
@@ -51,6 +51,7 @@ class Asthma(DiseaseDefinition):
         ]
     
     timespan_heuristics = []
+    
     
     @property
     def event_heuristics(self):
@@ -236,13 +237,36 @@ class Asthma(DiseaseDefinition):
                
         return heuristic_list
         
+    def process_existing_cases(self, relevant_patients,dx_patient_events,rx_patient_rxevents):
+        
+        # Non-recurring condition - once you have it, you've always 
+        existing_cases = Case.objects.filter(
+                patient__in = relevant_patients ,
+                condition = self.conditions[0],
+                ).select_related().order_by('date')
+
+        #get distinct patients 
+        patients = set()
+        for case in existing_cases:
+            events = []
+            patients.add(case.patient)
+            try:
+                events += rx_patient_rxevents[case.patient.id]
+                if dx_patient_events:
+                    events += dx_patient_events[case.patient.id]
+            except KeyError:
+                pass                
+            self._update_case_from_event_list(case,
+                            relevant_events = events )
+        return patients
+        
     def generate_def_ab (self):
     #
     # criteria 1
     # >=4 encounters with dx code 493.xx and asthma drug prescriptions 
     # (can be >=2 scripts for the same med or scripts for 2 or more different meds)
     #
-       
+           
         dx_ev_names = ['dx:asthma']
         rx_ev_names = [
             'rx:albuterol',
@@ -283,116 +307,88 @@ class Asthma(DiseaseDefinition):
              'rx:mometasone-formeterol:trade',
              'rx:budesonide-formeterol:trade',
             ]
-        log.info('Generating cases for Asthma Definition (a)')
+        log.info('Generating cases for Asthma new Definition (a)')
         counter_a = 0
         allrx_event_names =  rx_ev_names + rx_comb_ev_names
                
-        #TODO exclude all patients with asthma already 
         # possibly split in two sections for new and added events to existing cases 
         #or run two checks the dxs and rxs walk through the two lists.       
-        dx_qs = BaseEventHeuristic.get_events_by_name(dx_ev_names)
-        dx_qs = dx_qs.exclude(case__condition=self.conditions[0])
-        dx_qs = dx_qs.order_by('patient', 'date')
-        #TODO get distincts
-        new_patient = None
-        prev_patient = None
-        count_dx4 = 0
-        dx_count = dx_qs.count()
+        dx_qs = BaseEventHeuristic.get_events_by_name(name=dx_ev_names).exclude(case__condition=self.conditions[0]).order_by('date').select_related()
+        #get distinct patients 
+        dx_patients = set()
+        for event in dx_qs:
+            dx_patients.add(event.patient)
+            
+        #initializing counts for patients
+        dx_patient_events = {}
+        for event in dx_qs:
+            try:
+                dx_patient_events[event.patient_id].append(event)
+            except:
+                dx_patient_events[event.patient_id] = [event]
         
-        '''
-        for patient in dx_qs_patient:
-            dx4_event_qs = dx_qs.filter(patient = patient).count()
-            if dx4_event_qs>=4:
-                rx_qs = BaseEventHeuristic.get_events_by_name(name=allrx_event_names)
-                rx_qs = rx_qs.exclude(case__condition=self.conditions[0]) 
-                rx_qs = rx_qs.filter(patient=prev_patient)
-                if rx_qs.count() >= 2: 
-                    create new case 
-         '''   
-        ifcdx4 = 0    
-        for dx_event in dx_qs:  
-            new_patient = dx_event.patient
-            # first time, change of patient or last event in qs.
-            if not prev_patient or prev_patient != new_patient or  dx_event == dx_qs[dx_count-1]: 
-                if count_dx4 >= 4:
-                    #count how many times we get in here ? 
-                    #log.info('def a Start of if countdx4: %s' % datetime.datetime.now())
-                    ifcdx4 +=1
-                    rx_qs = BaseEventHeuristic.get_events_by_name(name=allrx_event_names)
-                    rx_qs = rx_qs.exclude(case__condition=self.conditions[0]) 
-                    rx_qs = rx_qs.filter(patient=prev_patient)
-                    if rx_qs.count() >= 2: 
-                        dx4_event_qs = dx_qs.filter(patient = prev_patient)
-                        #
-                        # Patient has Asthma
-                        #
-                        
-                        t, new_case = self._create_case_from_event_obj(
-                            condition = self.conditions[0],
-                            criteria = 'Criteria #1: Asthma diagnosis >=4 and >=2 prescriptions',
-                            recurrence_interval = None, # Does not recur
-                            event_obj = dx4_event,
-                            relevant_event_qs = rx_qs | dx4_event_qs,
-                            )
-                        if t:
-                            counter_a += 1
-                            log.info('Created new asthma case def a: %s' % new_case)
-                    #log.info('def a End of if countdx4: %s' % datetime.datetime.now())      
-                prev_patient = dx_event.patient 
-                #log.info('def a if count times: %s' % ifcdx4)    
-                
-                count_dx4 = 0  
-                dx4_event = dx_event
-                
-            count_dx4 +=1   
-            if (counter_a % 100)==0:
-                transaction.commit()           
- 
-        # criteria 2
-        # >=4 asthma drug dispensing events (any combination of multiple scripts
-        # for the same med or scripts for different meds
-        log.info('Generating cases for Asthma Definition (b)')
-        counter_b = 0
-        allrx_event_names =  rx_ev_names + rx_comb_ev_names
-               
-        mainrx_qs = BaseEventHeuristic.get_events_by_name(allrx_event_names)
-        mainrx_qs = mainrx_qs.exclude(case__condition=self.conditions[0]).order_by('patient', 'date')
-        new_patient = None
-        prev_patient = None
-        count_rx4 = 0
-        rx_count = mainrx_qs.count()
-        ifcrx4 = 0  
-        for rx_event in mainrx_qs:            
-            new_patient = rx_event.patient    
-            # TODO check for performance of mainrx_qs[rx_count-1]
-            if not prev_patient or prev_patient != new_patient or rx_event == mainrx_qs[rx_count-1]: 
-                if count_rx4 >= 4:  
-                    #log.info('def b Start of if countdx4: %s' % datetime.datetime.now())    
-                    ifcrx4 +=1
-                    rx4_event_qs =  mainrx_qs.filter(patient =prev_patient )       
+        rx_qs = BaseEventHeuristic.get_events_by_name(name=allrx_event_names).exclude(case__condition=self.conditions[0]).select_related()
+        # distinct patients
+        rx_patients = set()
+        for event in rx_qs:
+            rx_patients.add(event.patient)
+            
+        rx_patient_rxevents= {}
+        for event in rx_qs:
+            try:
+                rx_patient_rxevents[event.patient_id].append(event)
+            except:
+                rx_patient_rxevents[event.patient_id] = [event]
+    
+        patients_with_existing_cases = self.process_existing_cases(rx_patients.union(dx_patients),dx_patient_events,rx_patient_rxevents)
+        
+        for patient in dx_patients.intersection(rx_patients) - set(patients_with_existing_cases): 
+            
+            if len(dx_patient_events[patient.id]) >=4 and len(rx_patient_rxevents[patient.id]) >= 2: 
                     #
                     # Patient has Asthma
                     #
-                    t, new_case = self._create_case_from_event_obj(
-                        condition = self.conditions[0],
-                        criteria = 'Criteria #2: >=4 prescriptions',
-                        recurrence_interval = None, # Does not recur
-                        event_obj = rx4_event,
-                        relevant_event_qs =  rx4_event_qs,
-                        )
-                    if t: 
+                    t, new_case = self._create_case_from_event_list(
+                            condition = self.conditions[0],
+                            criteria = 'Criteria #1: Asthma new diagnosis >=4 and >=2 prescriptions',
+                            recurrence_interval = None, # Does not recur
+                            event_obj = dx_patient_events[patient.id][0],
+                            relevant_event_names = rx_patient_rxevents[patient.id] + dx_patient_events[patient.id],
+                            )
+                    if t:
+                        counter_a += 1
+                        log.info('Created new asthma new case def a: %s' % new_case)
+            if (counter_a % 100)==0:
+                transaction.commit() 
+         
+       
+        # criteria 2
+        # >=4 asthma drug dispensing events (any combination of multiple scripts
+        # for the same med or scripts for different meds
+        log.info('Generating cases for Asthma new Definition (b)')
+        counter_b = 0
+        #check again because some cases might have been created above.
+        patients_with_existing_cases = self.process_existing_cases(rx_patients.union(dx_patients),None,rx_patient_rxevents)
+        
+        for patient in rx_patients - set(patients_with_existing_cases):
+            if len(rx_patient_rxevents[patient.id])  >= 4: 
+                    #
+                    # Patient has Asthma
+                    #
+                    t, new_case = self._create_case_from_event_list(
+                            condition = self.conditions[0],
+                            criteria = 'Criteria #2: >=4 prescriptions',
+                            recurrence_interval = None, # Does not recur
+                            event_obj = rx_patient_rxevents[patient.id][0],
+                            relevant_event_names = rx_patient_rxevents[patient.id],
+                            )
+                    if t:
                         counter_b += 1
-                        log.info('Created new asthma case def b: %s' % new_case)
-                    #log.info('def b End of if countdx4: %s' % datetime.datetime.now())  
-                prev_patient = rx_event.patient  
-                count_rx4 = 0 
-                #log.info('def b if count times: %s' % ifcrx4)    
-                
-                rx4_event = rx_event
-                
-            count_rx4 +=1   
+                        log.info('Created new asthma new case def b: %s' % new_case)
             if (counter_b % 100)==0:
-                transaction.commit()            
+                    transaction.commit()   
+         
+                     
         return counter_a + counter_b # Count of new cases
     
     @transaction.commit_manually
@@ -401,7 +397,7 @@ class Asthma(DiseaseDefinition):
         counter = 0
         counter += self.generate_def_ab()
         transaction.commit()
-        log.debug('Generated %s new cases of asthma' % counter)
+        log.debug('Generated %s new cases of asthma new' % counter)
         
         return counter # Count of new cases
     
@@ -411,10 +407,9 @@ class Asthma(DiseaseDefinition):
 #
 #-------------------------------------------------------------------------------
 
-asthma_definition = Asthma()
 
 def event_heuristics():
-    return asthma_definition.event_heuristics
+    return AsthmaNew().event_heuristics
 
 def disease_definitions():
-    return [asthma_definition]
+    return [AsthmaNew()]
